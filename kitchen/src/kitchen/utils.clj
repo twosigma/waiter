51ca990d7902e0f5319315c605ccd1e4ac5cb525
Daniel Posada <daniel.posada@twosigma.com>
2017-05-31 23:45:28 -0500

    Adds require of clojure.core.async to make lein uberjar work

--- kitchen/src/kitchen/utils.clj
+++ kitchen/src/kitchen/utils.clj
@@ -1,6 +1,7 @@
 (ns kitchen.utils
   (:require [clj-time.core :as t]
             [clj-time.format :as f]
+            [clojure.core.async :as async]
             [clojure.data.json :as json]
             [clojure.string :as str])
   (:import clojure.core.async.impl.channels.ManyToManyChannel
