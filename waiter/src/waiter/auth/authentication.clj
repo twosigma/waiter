5b749bf44b10b79a98143668aa56bac5ab86b5d0
Shams Imam <shams.imam@gmail.com>
2017-06-05 12:08:13 -0500

    -- rename launch-as-user to run-as-user

--- waiter/src/waiter/auth/authentication.clj
+++ waiter/src/waiter/auth/authentication.clj
@@ -29,10 +29,10 @@
 
 ;; An anonymous request does not contain any authentication information.
 ;; This is equivalent to granting everyone access to the resource.
-;; The anonymous authenticator attaches the principal of launch-as-user to the request.
-;; In particular, this enables requests to launch processes as launch-as-user.
+;; The anonymous authenticator attaches the principal of run-as-user to the request.
+;; In particular, this enables requests to launch processes as run-as-user.
 ;; Use of this authentication mechanism is strongly discouraged for production use.
-(defrecord AnonymousAuthenticator [launch-as-user]
+(defrecord AnonymousAuthenticator [run-as-user]
 
   Authenticator
 
@@ -45,13 +45,13 @@
   (create-auth-handler [_ request-handler]
     (fn anonymous-handler [request]
       (let [request' (assoc request
-                       :authorization/user launch-as-user
-                       :authenticated-principal launch-as-user)]
+                       :authorization/user run-as-user
+                       :authenticated-principal run-as-user)]
         (request-handler request')))))
 
 (defn anonymous-authenticator
   "Factory function for creating AnonymousAuthenticator"
-  [{:keys [launch-as-user]}]
+  [{:keys [run-as-user]}]
   (log/warn "use of AnonymousAuthenticator is strongly discouraged for production use:"
-            "requests will use principal" launch-as-user)
-  (->AnonymousAuthenticator launch-as-user))
+            "requests will use principal" run-as-user)
+  (->AnonymousAuthenticator run-as-user))
