05e466d0dad2ec6990f7c297f0f476a3fb2ceb38
Daniel Posada <daniel.posada@gmail.com>
2017-06-13 13:08:37 -0500

    Adds x-waiter-auth cookie to one-user authenticator (#24)
    * Adds x-waiter-auth cookie to one-user authenticator
    
    * Extracts helper function for handling the common auth stuff

--- waiter/src/waiter/auth/spnego.clj
+++ waiter/src/waiter/auth/spnego.clj
@@ -72,10 +72,6 @@
   [^GSSContext gss]
   (str (.getSrcName gss)))
 
-(defn add-cached-auth
-  [response password princ]
-  (cookie-support/add-encoded-cookie response password auth/AUTH-COOKIE-NAME [princ (System/currentTimeMillis)] 1))
-
 (defn get-auth-cookie-value
   [cookie-string]
   (cookie-support/cookie-value cookie-string auth/AUTH-COOKIE-NAME))
@@ -117,14 +113,9 @@
         (let [^GSSContext gss_context (gss-context-init)]
           (let [token (do-gss-auth-check gss_context req)]
             (if (.isEstablished gss_context)
-              (let [princ (gss-get-princ gss_context)
-                    resp (-> req
-                             (assoc
-                               :authenticated-principal princ
-                               :authorization/user (first (str/split princ #"@" 2)))
-                             (rh)
-                             (add-cached-auth password princ)
-                             (cookie-support/cookies-async-response))]
+              (let [principal (gss-get-princ gss_context)
+                    user (first (str/split principal #"@" 2))
+                    resp (auth/handle-request-auth rh req user principal password)]
                 (log/debug "Added cookies to response")
                 (if token
                   (if (map? resp)
