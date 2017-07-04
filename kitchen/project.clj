77abce7cfb55c468b4cc715c402c720ca1b35ae8
Shams Imam <shams.imam@gmail.com>
2017-07-04 01:35:23 -0500

    sync-ing latest changes to kitchen

--- kitchen/project.clj
+++ kitchen/project.clj
@@ -1,3 +1,13 @@
+;;
+;;       Copyright (c) 2017 Two Sigma Investments, LP.
+;;       All Rights Reserved
+;;
+;;       THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF
+;;       Two Sigma Investments, LP.
+;;
+;;       The copyright notice above does not evidence any
+;;       actual or intended publication of such source code.
+;;
 (defproject kitchen "0.1.0-SNAPSHOT"
   :dependencies [^{:voom {:repo "https://github.com/twosigma/jet.git" :branch "waiter-patch"}}
                  [cc.qbits/jet "0.7.10-20170524_203939-gd23596c"]
@@ -10,7 +20,8 @@
                  [org.clojure/tools.cli "0.3.3"]
                  [org.clojure/tools.logging "0.3.1"]
                  [prismatic/plumbing "0.5.3"]
-                 [ring/ring-core "1.4.0"]]
+                 [ring/ring-core "1.4.0"]
+                 [ring-basic-authentication "1.0.5"]]
   :plugins [[lein-voom "0.1.0-20150115_230705-gd96d771"]]
   :main ^:skip-aot kitchen.core
   :target-path "target/%s"
