d7c9498bf26ce03b5c6d5811e136cf1f0851c8ca
Steven Radack <sradack@gmail.com>
2017-08-23 21:29:10 -0500

    Remove example usage

--- waiter/bin/add-loopback-ips-mac.sh
+++ waiter/bin/add-loopback-ips-mac.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 # Usage: add-loopback-ips-mac.sh
 #
-# Examples:
-#   add-loopback-ips-mac.sh
-#
 # Aliases 127.0.0.[2-10] address to lo0, which is needed to use shell scheduler locally.
 # This script is for Mac OS X.  Linux binds the necessary loopback IPs by default.
 
