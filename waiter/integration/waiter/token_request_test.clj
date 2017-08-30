eff4c50a672eddb8b94bfd015486655874286503
Steven Radack <sradack@gmail.com>
2017-08-30 15:24:55 -0500

    More explicit tests for x-waiter-consent

--- waiter/integration/waiter/token_request_test.clj
+++ waiter/integration/waiter/token_request_test.clj
@@ -559,7 +559,8 @@
                                                 "service-id" service-id})]
                   (reset! cookies-atom cookies)
                   (is (= "Added cookie x-waiter-consent" body))
-                  (is (= (set (map :name cookies)) #{"x-waiter-auth" "x-waiter-consent"}))
+                  ; x-waiter-consent should be emitted Waiter
+                  (is (contains? (set (map :name cookies)) "x-waiter-consent"))
                   (assert-response-status response 200)))
 
               (testing "auto run-as-user population on expected service-id"
@@ -574,7 +575,7 @@
                       (reset! service-id-atom service-id)
                       (is (= "Hello World" body))
                       ; x-waiter-consent should not be re-emitted Waiter
-                      (is (= (set (map :name cookies)) #{"x-waiter-auth"}))
+                      (is (not (contains? (set (map :name cookies)) "x-waiter-consent")))
                       (is (= expected-service-id service-id))
                       (is (not (str/blank? permitted-user)))
                       (is (= run-as-user permitted-user))
@@ -606,10 +607,10 @@
                                               "x-requested-with" "XMLHttpRequest"}
                                     :http-method-fn http/post
                                     :multipart {"mode" "token"})]
-                  (is (not= @cookies-atom cookies))
                   (reset! cookies-atom cookies)
                   (is (= "Added cookie x-waiter-consent" body))
-                  (is (= (set (map :name cookies)) #{"x-waiter-auth" "x-waiter-consent"}))
+                  ; x-waiter-consent should be emitted Waiter
+                  (is (contains? (set (map :name cookies)) "x-waiter-consent"))
                   (assert-response-status response 200)))
 
               (testing "auto run-as-user population on approved token"
@@ -622,7 +623,7 @@
                       (reset! service-id-atom service-id)
                       (is (= "Hello World" body))
                       ; x-waiter-consent should not be re-emitted Waiter
-                      (is (= (set (map :name cookies)) #{"x-waiter-auth"}))
+                      (is (not (contains? (set (map :name cookies)) "x-waiter-consent")))
                       (is (not= previous-service-id service-id))
                       (assert-response-status response 200))
                     (finally
