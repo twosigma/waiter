a21fdf095e8392572891cb0a8469cae831baa474
Steven Radack <sradack@gmail.com>
2017-09-18 15:26:04 -0500

    Dummy commit to trigger voom version

--- waiter/integration/waiter/basic_test.clj
+++ waiter/integration/waiter/basic_test.clj
@@ -528,6 +528,7 @@
             {:strs [waiter-error]} (try (json/read-str body)
                                         (catch Throwable e
                                           (is false (str "Could not parse body that is supposed to be JSON:\n" body))))] 
+
         (is (= 400 status))
         (is (= "application/json" (get headers "content-type")))
         (is waiter-error (str "Could not find waiter-error element in body " body))
