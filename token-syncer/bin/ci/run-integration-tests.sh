c2d3ee0e1dd9fe6e630263df8c91cb159b4f1ab5
Shams Imam <shams.imam@gmail.com>
2017-09-22 15:56:39 -0500

    -- use three waiter servers for token-syncer testing

--- token-syncer/bin/ci/run-integration-tests.sh
+++ token-syncer/bin/ci/run-integration-tests.sh
@@ -10,6 +10,17 @@
 
 set -ev
 
+function wait_for_server {
+    URI=${1}
+    while ! curl -s ${URI} >/dev/null;
+    do
+        echo "$(date +%H:%M:%S) server is not listening on ${URI} yet"
+        sleep 2.0
+    done
+    echo "$(date +%H:%M:%S) connected to server on ${URI}!"
+}
+export -f wait_for_server
+
 TEST_COMMAND=${1:-parallel-test}
 TEST_SELECTOR=${2:-integration}
 
@@ -17,15 +28,18 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 WAITER_DIR=${DIR}/../../../waiter
 SYNCER_DIR=${WAITER_DIR}/../token-syncer
 
-# Start waiter
-WAITER_PORT_1=9091
-${WAITER_DIR}/bin/run-using-shell-scheduler.sh ${WAITER_PORT_1} &
-
-WAITER_PORT_2=9092
-${WAITER_DIR}/bin/run-using-shell-scheduler.sh ${WAITER_PORT_2} &
+# Start waiter servers
+WAITER_URIS=""
+for waiter_port in 9093 9092 9091
+do
+  ${WAITER_DIR}/bin/run-using-shell-scheduler.sh ${waiter_port} &
+  WAITER_URI="http://127.0.0.1:${waiter_port}"
+  timeout 180s bash -c "wait_for_server ${WAITER_URI}"
+  WAITER_URIS="${WAITER_URI};${WAITER_URIS}"
+done
 
 # Run the integration tests
-export WAITER_URIS="http://127.0.0.1:${WAITER_PORT_1};http://127.0.0.1:${WAITER_PORT_2}"
+export WAITER_URIS="${WAITER_URIS%?}"
 ${SYNCER_DIR}/bin/test.sh ${TEST_COMMAND} ${TEST_SELECTOR} || test_failures=true
 
 # If there were failures, dump the logs
