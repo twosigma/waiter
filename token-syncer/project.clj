c698d67bc8951aaf57fd47539170700e5cbafb1d
Shams Imam <shams.imam@gmail.com>
2018-02-15 16:59:44 -0600

    adds support for spnego profile in project.clj

--- token-syncer/project.clj
+++ token-syncer/project.clj
@@ -19,9 +19,17 @@
                  [org.clojure/tools.logging "0.3.1"]
                  [org.slf4j/slf4j-log4j12 "1.7.21"]
                  [prismatic/plumbing "0.5.4"]]
+  :jvm-opts ["-server"
+             "-XX:+UseG1GC"
+             "-XX:MaxGCPauseMillis=50"]
   :main ^:skip-aot token-syncer.main
   :plugins [[lein-voom "0.1.0-20171225_233657-g7962d1d"]]
   :profiles {:default {:jvm-opts ["-Dlog4j.configuration=log4j.properties"]}
+             :spnego {:jvm-opts ["-Djava.security.krb5.conf=/etc/krb5.conf"
+                                 "-Djavax.security.auth.useSubjectCredsOnly=false"
+                                 "-Dsun.security.jgss.lib=/opt/mitkrb5/lib/libgssapi_krb5.so"
+                                 "-Dsun.security.jgss.native=true"
+                                 "-Dsun.security.krb5.debug=true"]}
              :test {:jvm-opts ["-Dlog4j.configuration=log4j-test.properties"]}
              :uberjar {:aot :all}}
   :resource-paths ["resources"]
