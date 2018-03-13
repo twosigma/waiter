5dccdfd39c18063152200f8be48c22c8107938de
Steven Radack <sradack@gmail.com>
2018-03-12 19:33:29 -0500

    Increase grace period for kitchen test apps

--- waiter/src/waiter/client_tools.clj
+++ waiter/src/waiter/client_tools.clj
@@ -319,6 +319,7 @@
   (let [headers (cond->
                   (-> {:x-waiter-cpus 0.1
                        :x-waiter-mem 256
+                       :x-waiter-grace-period-secs 120
                        :x-waiter-health-check-url "/status"
                        :x-waiter-idle-timeout-mins 10}
                       (merge custom-headers)
