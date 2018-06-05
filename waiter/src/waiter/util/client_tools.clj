5f98b2478c3f2b1659c81933b21ab09c47fdaba6
Nick Vrvilo <nick.vrvilo@alumni.rice.edu>
2018-06-05 16:57:54 +0000

    Lift current-test-name utility function (#343)
    * Lift current-test-name utility function
    
    * feedback from dpo

--- waiter/src/waiter/util/client_tools.clj
+++ waiter/src/waiter/util/client_tools.clj
@@ -163,9 +163,14 @@
     (.setCookieStore client (HttpCookieStore$Empty.))
     client))
 
+(defn current-test-name
+  "Get the name of the currently-running test."
+  []
+  (-> *testing-vars* first meta :name (or "test-unknown-name") str))
+
 (defmacro testing-using-waiter-url
   [& body]
-  `(let [name# (str (or (:name (meta (first *testing-vars*))) "test-unknown-name"))]
+  `(let [name# (current-test-name)]
      (testing name#
        (cid/with-correlation-id
          name#
