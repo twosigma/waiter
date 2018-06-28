cc7b831e6d0cd1a5e8d3192d34b943abff5342fc
Shams Imam <shams.imam@gmail.com>
2018-06-28 05:38:38 -0500

    moves retrieve-log-url and retrieve-directory-content-from-host to mesos.clj from mararthon.clj

--- waiter/src/waiter/mesos/mesos.clj
+++ waiter/src/waiter/mesos/mesos.clj
@@ -14,7 +14,8 @@
 ;; limitations under the License.
 ;;
 (ns waiter.mesos.mesos
-  (:require [waiter.util.http-utils :as http-utils])
+  (:require [clojure.string :as str]
+            [waiter.util.http-utils :as http-utils])
   (:import org.eclipse.jetty.client.HttpClient))
 
 (defrecord MesosApi [^HttpClient http-client spnego-auth slave-port slave-directory])
@@ -46,6 +47,20 @@
   (when (and slave-port host directory)
     (str "http://" host ":" slave-port "/files/download?path=" directory)))
 
+(defn retrieve-directory-content-from-host
+  "Retrieve the content of the directory for the given instance on the specified agent."
+  [mesos-api host directory]
+  (let [response-parsed (list-directory-content mesos-api host directory)]
+    (map (fn [entry]
+           (merge {:name (subs (:path entry) (inc (count directory)))
+                   :size (:size entry)}
+                  (if (= (:nlink entry) 1)
+                    {:type "file"
+                     :url (build-directory-download-link mesos-api host (:path entry))}
+                    {:type "directory"
+                     :path (:path entry)})))
+         response-parsed)))
+
 (defn get-agent-state
   "Returns information about the frameworks, executors and the agent’s master."
   [{:keys [http-client slave-port spnego-auth]} host]
@@ -54,3 +69,14 @@
                              :request-method :get
                              :spnego-auth spnego-auth
                              :throw-exceptions false)))
+
+(defn retrieve-log-url
+  "Retrieve the directory path for the specified task running on the specified agent."
+  [mesos-api task-id host framework-name]
+  (let [response-parsed (get-agent-state mesos-api host)
+        matching-frameworks (->> (concat (:completed_frameworks response-parsed) (:frameworks response-parsed))
+                                 (filter #(or (str/includes? (str/lower-case (:role %)) framework-name)
+                                              (str/includes? (str/lower-case (:name %)) framework-name))))
+        executors (mapcat #(concat (:completed_executors %) (:executors %)) matching-frameworks)
+        log-directory (str (:directory (first (filter #(= (:id %) task-id) executors))))]
+    log-directory))
