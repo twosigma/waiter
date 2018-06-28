bfb0fe1e5fe28b560c2ff59a05811378cac29586
Shams Imam <shams.imam@gmail.com>
2018-06-28 10:19:07 -0500

    feedback from @dposada

--- waiter/src/waiter/mesos/mesos.clj
+++ waiter/src/waiter/mesos/mesos.clj
@@ -47,19 +47,22 @@
   (when (and slave-port host directory)
     (str "http://" host ":" slave-port "/files/download?path=" directory)))
 
+(defn- process-directory-entry
+  "Converts an individual directory entry into a map representing the entry."
+  [mesos-api host directory {:keys [nlink path size]}]
+  (-> (if (= nlink 1)
+        {:type "file"
+         :url (build-directory-download-link mesos-api host path)}
+        {:path path
+         :type "directory"})
+      (assoc :name (subs path (inc (count directory)))
+             :size size)))
+
 (defn retrieve-directory-content-from-host
   "Retrieve the content of the directory for the given instance on the specified agent."
   [mesos-api host directory]
-  (let [response-parsed (list-directory-content mesos-api host directory)]
-    (map (fn [entry]
-           (merge {:name (subs (:path entry) (inc (count directory)))
-                   :size (:size entry)}
-                  (if (= (:nlink entry) 1)
-                    {:type "file"
-                     :url (build-directory-download-link mesos-api host (:path entry))}
-                    {:type "directory"
-                     :path (:path entry)})))
-         response-parsed)))
+  (->> (list-directory-content mesos-api host directory)
+       (map #(process-directory-entry mesos-api host directory %))))
 
 (defn get-agent-state
   "Returns information about the frameworks, executors and the agent’s master."
@@ -75,8 +78,8 @@
   [mesos-api task-id host framework-name]
   (let [response-parsed (get-agent-state mesos-api host)
         matching-frameworks (->> (concat (:completed_frameworks response-parsed) (:frameworks response-parsed))
-                                 (filter #(or (str/includes? (str/lower-case (:role %)) framework-name)
-                                              (str/includes? (str/lower-case (:name %)) framework-name))))
+                                 (filter #(or (-> % :name str str/lower-case (str/includes? framework-name))
+                                              (-> % :role str str/lower-case (str/includes? framework-name)))))
         executors (mapcat #(concat (:completed_executors %) (:executors %)) matching-frameworks)
         log-directory (str (:directory (first (filter #(= (:id %) task-id) executors))))]
     log-directory))
