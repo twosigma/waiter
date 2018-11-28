5a3c48b919d01e3f3e0416b21ff5c9b3b4abee6a
Shams <shamsimam@users.noreply.github.com>
2018-11-28 14:35:32 -0600

    removes reference to jobsystem

--- waiter/src/waiter/settings.clj
+++ waiter/src/waiter/settings.clj
@@ -256,7 +256,7 @@
               :invalid-service-description "Service description using waiter headers/token improperly configured"
               :not-enough-memory "Not enough memory allocated"
               :not-found "Not found"
-              :prestashed-tickets-not-available "Prestashed jobsystem tickets not available"}
+              :prestashed-tickets-not-available "Prestashed tickets not available"}
    :metric-group-mappings []
    :metrics-config {:inter-router-metrics-idle-timeout-ms 2000
                     :metrics-gc-interval-ms 60000
