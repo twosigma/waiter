fda5a3ea3adf1daeff3dfa70ce6693d555f10f0b
Nick Vrvilo <nick.vrvilo@alumni.rice.edu>
2019-02-20 16:21:34 -0600

    Add cpu bursting support to K8s pod specs (#572)

--- waiter/src/waiter/scheduler/kubernetes.clj
+++ waiter/src/waiter/scheduler/kubernetes.clj
@@ -734,8 +734,7 @@
                                                                :failureThreshold 1
                                                                :periodSeconds health-check-interval-secs
                                                                :timeoutSeconds 1}
-                                              :resources {:limits {:cpu cpus
-                                                                   :memory memory}
+                                              :resources {:limits {:memory memory}
                                                           :requests {:cpu cpus
                                                                      :memory memory}}
                                               :volumeMounts [{:mountPath work-path
@@ -757,7 +756,7 @@
            :imagePullPolicy "IfNotPresent"
            :name "waiter-fileserver"
            :ports [{:containerPort port}]
-           :resources {:limits {:cpu cpu :memory memory}
+           :resources {:limits {:memory memory}
                        :requests {:cpu cpu :memory memory}}
            :volumeMounts [{:mountPath "/srv/www"
                            :name "user-home"}]
