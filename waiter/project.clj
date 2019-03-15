a6fabea9f23e15807ddeb02101a3bd4877a806c1
Shams Imam <shams.imam@twosigma.com>
2019-03-15 08:47:00 -0500

    upgrades jet to 0.7.10-20190315_050957-g3e53036

--- waiter/project.clj
+++ waiter/project.clj
@@ -32,7 +32,7 @@
 
   :dependencies [[bidi "2.1.5"
                   :exclusions [prismatic/schema ring/ring-core]]
-                 [twosigma/jet "0.7.10-20190304_152154-g0d6366e"]
+                 [twosigma/jet "0.7.10-20190315_050957-g3e53036"]
                  [twosigma/clj-http "1.0.2-20180124_201819-gcdf23e5"
                   :exclusions [commons-codec commons-io org.clojure/tools.reader potemkin slingshot]]
                  [clj-time "0.15.1"
