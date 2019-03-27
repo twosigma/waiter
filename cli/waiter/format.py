c059d10d96eb156f5a2e5d52a9c33a937c888028
Daniel Posada <daniel.posada@gmail.com>
2019-03-27 17:23:05 -0500

    Improves display of a fiew fields in waiter show (#650)

--- cli/waiter/format.py
+++ cli/waiter/format.py
@@ -19,4 +19,12 @@ def format_timestamp_string(s):
 
 def format_field_name(s):
     """Formats the given field name in a more readable format"""
-    return s.capitalize().replace('-', ' ').replace('secs', '(seconds)')
+    parts = s.split('-')
+    if parts[-1] == 'secs':
+        parts[-1] = '(seconds)'
+    if parts[0] == 'min':
+        parts[0] = 'minimum'
+    elif parts[0] == 'max':
+        parts[0] = 'maximum'
+    with_spaces = ' '.join(parts)
+    return with_spaces.capitalize()
