3d98fa829e325a28b63ea43b982901387988ff81
Nikolai Sinkov <nsinkov@gmail.com>
2019-04-03 15:02:39 -0500

    restore exception-wrapping behavior that was removed by PR #613

--- waiter/src/waiter/service_description.clj
+++ waiter/src/waiter/service_description.clj
@@ -899,23 +899,28 @@
       (when-not (seq user-service-description)
         (throw (ex-info (utils/message :cannot-identify-service)
                         (error-message-map-fn passthrough-headers waiter-headers))))
-      (let [{:keys [core-service-description service-description service-id]}
-            (build service-description-builder user-service-description
-                   {:assoc-run-as-user-approved? assoc-run-as-user-approved?
-                    :defaults defaults
-                    :kv-store kv-store
-                    :metric-group-mappings metric-group-mappings
-                    :service-id-prefix service-id-prefix
-                    :username username})
-            service-preauthorized (and token-preauthorized (empty? service-description-based-on-headers))
-            service-authentication-disabled (and token-authentication-disabled (empty? service-description-based-on-headers))]
-        {:core-service-description core-service-description
-         :on-the-fly? contains-waiter-header?
-         :service-authentication-disabled service-authentication-disabled
-         :service-description service-description
-         :service-id service-id
-         :service-preauthorized service-preauthorized
-         :source-tokens source-tokens})))
+      (sling/try+
+        (let [{:keys [core-service-description service-description service-id]}
+              (build service-description-builder user-service-description
+                     {:assoc-run-as-user-approved? assoc-run-as-user-approved?
+                      :defaults defaults
+                      :kv-store kv-store
+                      :metric-group-mappings metric-group-mappings
+                      :service-id-prefix service-id-prefix
+                      :username username})
+              service-preauthorized (and token-preauthorized (empty? service-description-based-on-headers))
+              service-authentication-disabled (and token-authentication-disabled (empty? service-description-based-on-headers))]
+          {:core-service-description core-service-description
+           :on-the-fly? contains-waiter-header?
+           :service-authentication-disabled service-authentication-disabled
+           :service-description service-description
+           :service-id service-id
+           :service-preauthorized service-preauthorized
+           :source-tokens source-tokens})
+        (catch [:type :service-description-error] ex-data
+          (throw (ex-info (:message ex-data)
+                          (merge (error-message-map-fn passthrough-headers waiter-headers) ex-data)
+                          (:throwable &throw-context)))))))
 
   (defn validate-service-description
     "Returns nil if the provided descriptor contains a valid service description.
