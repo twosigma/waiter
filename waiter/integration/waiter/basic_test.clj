bf8e6e95c5ae2d0a786aec72678d86474824ac3b
Nikolai Sinkov <nsinkov@gmail.com>
2019-04-03 14:46:43 -0500

    PR feedback

--- waiter/integration/waiter/basic_test.clj
+++ waiter/integration/waiter/basic_test.clj
@@ -939,9 +939,7 @@
             (let [kitchen-image (System/getenv "INTEGRATION_TEST_KITCHEN_IMAGE")
                   _ (is (not (str/blank? kitchen-image)) "You must provide a kitchen image in the INTEGRATION_TEST_KITCHEN_IMAGE environment variable")]
               (make-kitchen-request-fn kitchen-image 200))
-            (using-cook? waiter-url)
-            (make-kitchen-request-fn "dummy/image" 500)
-            (using-marathon? waiter-url)
-            (make-kitchen-request-fn "dummy/image" 500)
-            (using-shell? waiter-url)
+            (or (using-cook? waiter-url)
+                (using-marathon? waiter-url)
+                (using-shell? waiter-url))
             (make-kitchen-request-fn "dummy/image" 500)))))
