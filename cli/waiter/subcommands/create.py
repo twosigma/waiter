662441ecf978a10a72de8aba7b95c4c5815dbf37
Daniel Posada <daniel.posada@gmail.com>
2019-04-05 20:39:14 -0500

    Removes unused function

--- cli/waiter/subcommands/create.py
+++ cli/waiter/subcommands/create.py
@@ -1,6 +1,4 @@
 from functools import partial
-import argparse
-import logging
 
 from waiter import token_post
 
@@ -14,15 +12,6 @@ def register(add_parser):
     parser = token_post.register_argument_parser(add_parser, action)
     token_post.add_arguments(parser)
     return partial(token_post.create_or_update_token, action=action)
-
-
-def boolean_string(s):
-    """Converts the given string to a boolean, or raises"""
-    b = str2bool(s)
-    if b is None:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
-    else:
-        return b
 
 
 def add_implicit_arguments(unknown_args):
