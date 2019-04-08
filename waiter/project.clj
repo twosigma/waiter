216c25fa9616b7953b27b0d158ae365ffcc5876d
Shams Imam <shams.imam@twosigma.com>
2019-04-08 10:53:05 -0500

    updates jet dependency for changes

--- waiter/project.clj
+++ waiter/project.clj
@@ -32,7 +32,7 @@
 
   :dependencies [[bidi "2.1.5"
                   :exclusions [prismatic/schema ring/ring-core]]
-                 [twosigma/jet "0.7.10-20190401_195326-g41a49b9"
+                 [twosigma/jet "0.7.10-20190408_155019-g2159945"
                   :exclusions [org.mortbay.jetty.alpn/alpn-boot]]
                  [twosigma/clj-http "1.0.2-20180124_201819-gcdf23e5"
                   :exclusions [commons-codec commons-io org.clojure/tools.reader potemkin slingshot]]
