aae887b398a6dfa860f699850b1c2e2d48b58457
Nikolai Sinkov <nsinkov@gmail.com>
2019-05-20 17:46:48 -0500

    rename dynamic authenticator

--- waiter/src/waiter/auth/composite.clj
+++ waiter/src/waiter/auth/composite.clj
@@ -13,36 +13,29 @@
 ;; See the License for the specific language governing permissions and
 ;; limitations under the License.
 ;;
-(ns waiter.auth.dynamic
+(ns waiter.auth.composite
   (:require [plumbing.core :as pc]
             [waiter.auth.authentication :as auth]
             [waiter.auth.saml :as saml]
             [waiter.util.utils :as utils]))
 
-(defrecord DynamicAuthenticator [authenticators saml-acs-handler-fn]
+(defrecord CompositeAuthenticator [authenticators saml-acs-handler-fn]
   auth/Authenticator
   (wrap-auth-handler [_ request-handler]
     (let [handlers (pc/map-vals #(auth/wrap-auth-handler % request-handler) authenticators)]
-      (fn dynamic-authenticator-handler [{{:strs [x-waiter-authentication]} :headers :as request}]
-        (let [authentication (get-in request [:waiter-discovery :service-parameter-template "authentication"])
-              authenticator-name (or authentication x-waiter-authentication)]
-          (when (and authentication x-waiter-authentication (not= authentication x-waiter-authentication))
-            (throw (ex-info "Both x-waiter-authentication header and authentication are set but are not the same."
-                            {:request-authentication authentication
-                             :request-x-waiter-authentication x-waiter-authentication
-                             :status 400})))
-          (if authenticator-name
-            (if-let [handler ((keyword authenticator-name) handlers)]
+      (fn composite-authenticator-handler [request]
+        (let [authentication (get-in request [:waiter-discovery :service-parameter-template "authentication"])]
+          (if authentication
+            (if-let [handler ((keyword authentication) handlers)]
               (handler request)
-              (throw (ex-info (str "No authenticator found for " authenticator-name " authentication.")
+              (throw (ex-info (str "No authenticator found for " authentication " authentication.")
                               {:request-authentication authentication
-                               :request-x-waiter-authentication x-waiter-authentication
-                               :dynamic-authenticators (keys authenticators)
+                               :composite-authenticators (keys authenticators)
                                :status 400})))
             ((:default handlers) request)))))))
 
-(defn dynamic-authenticator
-  "Factory function for creating dynamic authenticator middleware"
+(defn composite-authenticator
+  "Factory function for creating composite authenticator middleware"
   [{:keys [authenticator-config default-kind] :as context}]
   {:pre [(not-empty authenticator-config)
          (keyword? default-kind)
@@ -50,8 +43,8 @@
   (let [make-authenticator #(utils/create-component (assoc authenticator-config :kind %) :context context)
         authenticators (pc/map-from-keys
                          make-authenticator
-                         (filter #(not (contains? #{:kind :dynamic} %))
+                         (filter #(not (contains? #{:kind :composite} %))
                                  (keys authenticator-config)))
         saml-authenticator (:saml authenticators)]
-    (->DynamicAuthenticator (assoc authenticators :default (default-kind authenticators))
+    (->CompositeAuthenticator (assoc authenticators :default (default-kind authenticators))
                             (fn [request _] (saml/saml-acs-handler request saml-authenticator)))))
