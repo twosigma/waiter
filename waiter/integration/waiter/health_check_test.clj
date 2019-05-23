fbf47d94aa24d863fe345b3821d226bf771d2508
Shams Imam <shams.imam@twosigma.com>
2019-05-23 11:42:50 -0500

    adds option to see delayed scheduler synced results in ping

--- waiter/integration/waiter/health_check_test.clj
+++ waiter/integration/waiter/health_check_test.clj
@@ -31,7 +31,9 @@
                (get-in ping-response [:headers :x-kitchen-protocol-version]))
             (str ping-response))
         (is (= "get" (get-in ping-response [:headers :x-kitchen-request-method])) (str ping-response))
-        (is (= {:exists? true :healthy? true :service-id service-id :status "Running"} service-state)))
+        (is (or (= {:exists? true :healthy? true :service-id service-id :status "Running"} service-state)
+                (= {:exists? true :healthy? false :service-id service-id :status "Starting"} service-state))
+            (str service-state)))
       (do
         (is (= "timed-out" (get ping-response :result)) (str ping-response))
         (is (= {:exists? true :healthy? false :service-id service-id :status "Starting"} service-state))))))
