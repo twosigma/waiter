544d29db77b09a63b009921c9ef346e445a9975f
Nikolai Sinkov <nsinkov@gmail.com>
2019-05-28 17:43:03 -0500

    switch saml test to fast

--- waiter/integration/waiter/saml_authentication_test.clj
+++ waiter/integration/waiter/saml_authentication_test.clj
@@ -24,25 +24,13 @@
         cookie-jar-path (.getAbsolutePath cookie-jar-file)
         curl-output-file (java.io.File/createTempFile "curl-output" ".txt")
         curl-output-path (.getAbsolutePath curl-output-file)
-        _ (println "xxxzzz")
-        _ (println (str "curl '" saml-redirect-location "' -k -c " cookie-jar-path " -L -v > " curl-output-path))
-        xxx (shell/sh "bash" "-c" (str "curl '" saml-redirect-location "' -k -c " cookie-jar-path " -L -v > " curl-output-path))
-        _ (println xxx)
-        _ (println (slurp curl-output-path))
-        curl-verbose-info (:err xxx)
-        ;curl-verbose-info (:err (shell/sh "bash" "-c" (str "curl '" saml-redirect-location "' -k -c " cookie-jar-path " -L -v > " curl-output-path)))
+        curl-verbose-info (:err (shell/sh "bash" "-c" (str "curl '" saml-redirect-location "' -k -c " cookie-jar-path " -L -v > " curl-output-path)))
         login-form-location (second (re-matches #"(?ms).*Location: ([^?]+).*" curl-verbose-info))
         {:keys [login-form-action auth-state]}
         (extract (parse (slurp curl-output-path)) [:login-form-action :auth-state]
                  "form" (attr :action)
                  "form input[name=AuthState]" (attr :value))
-        _ (println (str "curl '" (str login-form-location login-form-action) "' -k -b " cookie-jar-path " -F 'AuthState=" auth-state "' -F 'username=user2' -F 'password=user2pass' > " curl-output-path))
-        xxx (shell/sh "bash" "-c" (str "curl '" (str login-form-location login-form-action) "' -k -b " cookie-jar-path " -F 'AuthState=" auth-state "' -F 'username=user2' -F 'password=user2pass' > " curl-output-path))
-        _ (println xxx)
-        _ (println (slurp curl-output-path))
-        _ (println "xxxzzzxxx")
-        _ (is (= 0 (:exit xxx)))
-        ;_ (is (= 0 (:exit (shell/sh "bash" "-c" (str "curl '" (str login-form-location login-form-action) "' -k -b " cookie-jar-path " -F 'AuthState=" auth-state "' -F 'username=user2' -F 'password=user2pass' > " curl-output-path)))))
+        _ (is (= 0 (:exit (shell/sh "bash" "-c" (str "curl '" (str login-form-location login-form-action) "' -k -b " cookie-jar-path " -F 'AuthState=" auth-state "' -F 'username=user2' -F 'password=user2pass' > " curl-output-path)))))
         {:keys [waiter-saml-acs-endpoint saml-response relay-state]}
         (extract (parse (slurp curl-output-path)) [:waiter-saml-acs-endpoint :saml-response :relay-state]
                  "form" (attr :action)
@@ -70,7 +58,7 @@
         _ (.delete cookie-jar-file)]
     {:relay-state relay-state :saml-response saml-response :waiter-saml-acs-endpoint waiter-saml-acs-endpoint}))
 
-(deftest ^:parallel ^:integration-slow ^:resource-heavy test-saml-authentication
+(deftest ^:parallel ^:integration-fast test-saml-authentication
   (testing-using-waiter-url
     (let [authenticator-kind (get-in (waiter-settings waiter-url) [:authenticator-config :kind])]
       (when (= "composite" authenticator-kind)
