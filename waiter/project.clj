89eeeefbaf5f0a9f6e6fe7fb3ae6727c9e916aba
Shams Imam <shams.imam@twosigma.com>
2019-06-18 20:59:18 -0500

    Revert "upgrades to jetty version 9.4.19.v20190610 (#797)"
    This reverts commit c8fa246f

--- waiter/project.clj
+++ waiter/project.clj
@@ -41,7 +41,7 @@
                  [io.grpc/grpc-core "1.20.0"
                   :exclusions [com.google.guava/guava]
                   :scope "test"]
-                 [twosigma/jet "0.7.10-20190612_154727-g9cb249e"
+                 [twosigma/jet "0.7.10-20190610_212308-g550b820"
                   :exclusions [org.mortbay.jetty.alpn/alpn-boot]]
                  [twosigma/clj-http "1.0.2-20180124_201819-gcdf23e5"
                   :exclusions [commons-codec commons-io org.clojure/tools.reader potemkin slingshot]]
