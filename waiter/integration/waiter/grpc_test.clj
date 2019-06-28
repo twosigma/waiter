02e6397061204511d7f6d45214c7762483591420
Shams Imam <shams.imam@twosigma.com>
2019-06-28 02:46:10 -0500

    adds integration tests for client cancellation

--- waiter/integration/waiter/grpc_test.clj
+++ waiter/integration/waiter/grpc_test.clj
@@ -22,6 +22,11 @@
   (:import (com.twosigma.waiter.courier GrpcClient)
            (java.util.function Function)))
 
+;; initialize logging on the grpc client
+(GrpcClient/setLogFunction (reify Function
+                             (apply [_ message]
+                               (log/info message))))
+
 (defn- basic-grpc-service-parameters
   []
   (let [courier-command (courier-server-command "${PORT0} ${PORT1}")]
@@ -29,12 +34,15 @@
       {:x-waiter-backend-proto "h2c"
        :x-waiter-cmd courier-command
        :x-waiter-cmd-type "shell"
+       :x-waiter-concurrency-level 32
        :x-waiter-cpus 0.2
        :x-waiter-debug true
        :x-waiter-grace-period-secs 120
        :x-waiter-health-check-port-index 1
        :x-waiter-health-check-proto "http"
        :x-waiter-idle-timeout-mins 10
+       :x-waiter-max-instances 1
+       :x-waiter-min-instances 1
        :x-waiter-mem 512
        :x-waiter-name (rand-name)
        :x-waiter-ports 2
@@ -45,65 +53,238 @@
   [length]
   (apply str (take length (repeatedly #(char (+ (rand 26) 65))))))
 
-(deftest ^:parallel ^:integration-fast test-basic-grpc-server
+(defn ping-courier-service
+  [waiter-url request-headers]
+  (make-request waiter-url "/waiter-ping" :headers request-headers))
+
+(defn start-courier-instance
+  [waiter-url]
+  (let [[host _] (str/split waiter-url #":")
+        h2c-port (Integer/parseInt (retrieve-h2c-port waiter-url))
+        request-headers (basic-grpc-service-parameters)
+        {:keys [cookies headers] :as response} (ping-courier-service waiter-url request-headers)
+        cookie-header (str/join "; " (map #(str (:name %) "=" (:value %)) cookies))
+        service-id (get headers "x-waiter-service-id")
+        request-headers (assoc request-headers
+                          "cookie" cookie-header
+                          "x-waiter-timeout" "60000")]
+    (assert-response-status response 200)
+    (is service-id)
+    (log/info "service-id:" service-id)
+    (let [{:keys [ping-response service-state]} (some-> response :body try-parse-json walk/keywordize-keys)]
+      (is (= "received-response" (:result ping-response)) (str ping-response))
+      (is (str/starts-with? (str (some-> ping-response :headers :server)) "courier-health-check"))
+      (assert-response-status ping-response 200)
+      (is (or (= {:exists? true :healthy? true :service-id service-id :status "Running"} service-state)
+              (= {:exists? true :healthy? false :service-id service-id :status "Starting"} service-state))
+          (str service-state)))
+    (assert-service-on-all-routers waiter-url service-id cookies)
+
+    {:cookies cookies
+     :h2c-port h2c-port
+     :host host
+     :request-headers request-headers
+     :service-id service-id}))
+
+(defn- retrieve-states
+  "Retrieves the states corresponding to a cid in the gRPC service defined by the request headers."
+  [host h2c-port request-headers service-id correlation-id expect-cancel?]
+  (let [retrieve-state-cid (rand-name)
+        request-headers (assoc request-headers "x-cid" retrieve-state-cid)
+        response (GrpcClient/retrieveState host h2c-port request-headers correlation-id)
+        state-count (or (some-> response (.getStateCount)) 0)
+        states (vec (doall (map #(.getState response %) (range state-count))))
+        assertion-message (str {:retrieve-cid retrieve-state-cid
+                                :result {:correlation-id correlation-id
+                                         :states states}
+                                :service-id service-id})]
+    (log/info correlation-id "states:" states)
+    (cond
+      ;; request executed without sending any data to the backend should be in non-lock-step mode
+      (zero? (count states))
+      (is (str/includes? correlation-id "independent") assertion-message)
+
+      ;; request executed without sending any data to the backend should be in non-lock-step mode
+      (= 2 (count states))
+      (do
+        (is (str/includes? correlation-id "independent") assertion-message)
+        (is (= ["INIT" "READY"] states) assertion-message))
+
+      ;; request executed without sending any data to the backend should be in non-lock-step mode
+      (= 3 (count states))
+      (do
+        (is (str/includes? correlation-id "independent") assertion-message)
+        (is (= ["INIT" "CANCEL" "CLOSE"] states) assertion-message))
+
+      ;; request executed without sending any data to the backend should be in non-lock-step mode
+      (and (= 5 (count states))
+           (not-any? #(= "RECEIVE_MESSAGE" %) states))
+      (do
+        (is (str/includes? correlation-id "independent") assertion-message)
+        (is (= ["INIT" "READY" "HALF_CLOSE" "CLOSE" "COMPLETE"] states) assertion-message))
+
+      ;; request connected to the backend and transmitted some data
+      ;; we must see a cancellation state for such requests
+      :else
+      (do
+        (when-not (= ["INIT" "READY"] (take 2 states))
+          (println assertion-message)
+          (log/info "FAIL:" assertion-message))
+        (if (> (count states) 6)
+          (is (= ["INIT" "READY" "RECEIVE_MESSAGE" "SEND_HEADERS"] (take 4 states)) assertion-message)
+          (is (= ["INIT" "READY"] (take 2 states)) assertion-message))
+        (when-not (every? #{"RECEIVE_MESSAGE" "SEND_MESSAGE"} (->> states (drop 4) (drop-last 4)))
+          (println assertion-message)
+          (log/info "FAIL:" assertion-message))
+        (is (every? #{"RECEIVE_MESSAGE" "SEND_MESSAGE"} (->> states (drop 4) (drop-last 4))) assertion-message)
+        (if expect-cancel?
+          ;; abnormal / cancellation path
+          (let [expected-last-two-state-members #{"CLOSE" "CANCEL" "COMPLETE"}
+                actual-last-two-states (take-last 2 states)]
+            (is (every? expected-last-two-state-members actual-last-two-states) assertion-message)
+            (when-not (some #{"CANCEL"} actual-last-two-states)
+              (println assertion-message)
+              (log/info "FAIL:" assertion-message))
+            (is (some #{"CANCEL"} actual-last-two-states) assertion-message)
+            (is (= 2 (count (set actual-last-two-states))) assertion-message))
+          ;; normal path
+          (is (= ["SEND_MESSAGE" "HALF_CLOSE" "CLOSE" "COMPLETE"] (take-last 4 states)) assertion-message))))
+    states))
+
+(deftest ^:parallel ^:integration-fast test-grpc-unary-call
   (testing-using-waiter-url
-    (GrpcClient/setLogFunction (reify Function
-                                 (apply [_ message]
-                                   (log/info message))))
-    (let [[host _] (str/split waiter-url #":")
-          h2c-port (Integer/parseInt (retrieve-h2c-port waiter-url))
-          request-headers (basic-grpc-service-parameters)
-          {:keys [cookies headers] :as response} (make-request waiter-url "/waiter-ping" :headers request-headers)
-          cookie-header (str/join "; " (map #(str (:name %) "=" (:value %)) cookies))
-          service-id (get headers "x-waiter-service-id")]
-
-      (assert-response-status response 200)
-      (is service-id)
-
+    (let [{:keys [h2c-port host request-headers service-id]} (start-courier-instance waiter-url)]
       (with-service-cleanup
         service-id
-
-        (let [{:keys [ping-response service-state]} (some-> response :body try-parse-json walk/keywordize-keys)]
-          (is (= "received-response" (:result ping-response)) (str ping-response))
-          (is (str/starts-with? (str (some-> ping-response :headers :server)) "courier-health-check"))
-          (assert-response-status ping-response 200)
-          (is (or (= {:exists? true :healthy? true :service-id service-id :status "Running"} service-state)
-                  (= {:exists? true :healthy? false :service-id service-id :status "Starting"} service-state))
-              (str service-state)))
-
         (testing "small request and reply"
           (log/info "starting small request and reply test")
           (let [id (rand-name "m")
                 from (rand-name "f")
                 content (rand-str 1000)
-                request-headers (assoc request-headers "cookie" cookie-header "x-cid" (rand-name))
+                request-headers (assoc request-headers "x-cid" (rand-name))
                 reply (GrpcClient/sendPackage host h2c-port request-headers id from content)]
             (is reply)
             (when reply
               (is (= id (.getId reply)))
               (is (= content (.getMessage reply)))
-              (is (= "received" (.getResponse reply))))))
+              (is (= "received" (.getResponse reply))))))))))
 
-        (testing "streaming to and from server"
-          (doseq [max-message-length [100 1000 10000 100000]]
-            (let [messages (doall (repeatedly 200 #(rand-str (inc (rand-int max-message-length)))))]
+(deftest ^:parallel ^:integration-fast test-grpc-streaming-successful
+  (testing-using-waiter-url
+    (let [{:keys [h2c-port host request-headers service-id]} (start-courier-instance waiter-url)]
+      (with-service-cleanup
+        service-id
+        (doseq [max-message-length [1000 10000 100000]]
+          (let [num-messages 100
+                messages (doall (repeatedly num-messages #(rand-str (inc (rand-int max-message-length)))))]
 
-              (testing (str "independent mode " max-message-length " messages")
+            (testing (str "independent mode " max-message-length " messages completion")
+              (log/info "starting streaming to and from server - independent mode test")
+              (let [cancel-threshold (inc num-messages)
+                    from (rand-name "f")
+                    collect-cid (str (rand-name) "-independent-complete")
+                    request-headers (assoc request-headers "x-cid" collect-cid)
+                    summaries (GrpcClient/collectPackages
+                                host h2c-port request-headers "m-" from messages 10 false cancel-threshold)]
+                (log/info collect-cid "collecting independent packages...")
+                (is (= (count messages) (count summaries)))
+                (when (seq summaries)
+                  (is (= (range 1 (inc (count messages))) (map #(.getNumMessages %) summaries)))
+                  (is (= (reductions + (map count messages)) (map #(.getTotalLength %) summaries))))
+                (Thread/sleep 1000) ;; allow server-side error processing to complete
+                (let [states (retrieve-states host h2c-port request-headers service-id collect-cid false)]
+                  (is (= (+ 6 (* 2 num-messages)) (count states)) (str collect-cid " -> " states)))))
+
+            (testing (str "lock-step mode " max-message-length " messages completion")
+              (log/info "starting streaming to and from server - lock-step mode test")
+              (let [cancel-threshold (inc num-messages)
+                    from (rand-name "f")
+                    collect-cid (str (rand-name) "-lock-step-complete")
+                    request-headers (assoc request-headers "x-cid" collect-cid)
+                    summaries (GrpcClient/collectPackages
+                                host h2c-port request-headers "m-" from messages 1 true cancel-threshold)]
+                (log/info collect-cid "collecting lock-step packages...")
+                (is (= (count messages) (count summaries)))
+                (when (seq summaries)
+                  (is (= (range 1 (inc (count messages))) (map #(.getNumMessages %) summaries)))
+                  (is (= (reductions + (map count messages)) (map #(.getTotalLength %) summaries))))
+                (Thread/sleep 1000) ;; allow server-side error processing to complete
+                (let [states (retrieve-states host h2c-port request-headers service-id collect-cid false)]
+                  (is (= (+ 6 (* 2 num-messages)) (count states)) (str collect-cid " -> " states)))))))))))
+
+(deftest ^:parallel ^:integration-slow test-grpc-streaming-client-cancellation
+  (testing-using-waiter-url
+    (let [{:keys [h2c-port host request-headers service-id]} (start-courier-instance waiter-url)]
+      (with-service-cleanup
+        service-id
+        (doseq [max-message-length [1000 10000 100000]]
+          (let [num-messages 100
+                messages (doall (repeatedly num-messages #(rand-str (inc (rand-int max-message-length)))))]
+
+            (dotimes [_ 10]
+              (testing (str "independent mode " max-message-length " messages cancellations")
                 (log/info "starting streaming to and from server - independent mode test")
-                (let [from (rand-name "f")
-                      request-headers (assoc request-headers "cookie" cookie-header "x-cid" (rand-name))
-                      summaries (GrpcClient/collectPackages host h2c-port request-headers "m-" from messages 1 false)]
-                  (is (= (count messages) (count summaries)))
-                  (when (seq summaries)
-                    (is (= (range 1 (inc (count messages))) (map #(.getNumMessages %) summaries)))
-                    (is (= (reductions + (map count messages)) (map #(.getTotalLength %) summaries))))))
+                (let [cancel-threshold (+ 5 (rand-int (- num-messages 5)))
+                      from (rand-name "f")
+                      collect-cid (str (rand-name) "-independent-cancel." cancel-threshold "." max-message-length)
+                      request-headers (assoc request-headers "x-cid" collect-cid)
+                      summaries (GrpcClient/collectPackages
+                                  host h2c-port request-headers "m-" from messages 1 false cancel-threshold)]
+                  (log/info collect-cid "collecting independent packages...")
+                  (is (nil? summaries))
+                  (Thread/sleep 1000) ;; allow server-side error processing to complete
+                  (let [states (retrieve-states host h2c-port request-headers service-id collect-cid true)]
+                    (is (>= (+ 6 (* 2 cancel-threshold)) (count states)) (str collect-cid " -> " states)))))
 
-              (testing (str "lock-step mode " max-message-length " messages")
+              (testing (str "lock-step mode " max-message-length " messages cancellations")
                 (log/info "starting streaming to and from server - lock-step mode test")
-                (let [from (rand-name "f")
-                      request-headers (assoc request-headers "cookie" cookie-header "x-cid" (rand-name))
-                      summaries (GrpcClient/collectPackages host h2c-port request-headers "m-" from messages 1 true)]
-                  (is (= (count messages) (count summaries)))
-                  (when (seq summaries)
-                    (is (= (range 1 (inc (count messages))) (map #(.getNumMessages %) summaries)))
-                    (is (= (reductions + (map count messages)) (map #(.getTotalLength %) summaries)))))))))))))
+                (let [cancel-threshold (+ 5 (rand-int (- num-messages 5)))
+                      from (rand-name "f")
+                      collect-cid (str (rand-name) "-lock-step-cancel." cancel-threshold "." max-message-length)
+                      request-headers (assoc request-headers "x-cid" collect-cid)
+                      summaries (GrpcClient/collectPackages
+                                  host h2c-port request-headers "m-" from messages 1 true cancel-threshold)]
+                  (log/info collect-cid "collecting lock-step packages...")
+                  (is (nil? summaries))
+                  (Thread/sleep 1000) ;; allow server-side error processing to complete
+                  (let [states (retrieve-states host h2c-port request-headers service-id collect-cid true)]
+                    (is (>= (+ 6 (* 2 cancel-threshold)) (count states)) (str collect-cid " -> " states))))))))))))
+
+(deftest ^:parallel ^:integration-slow test-grpc-streaming-server-cancellation
+  (testing-using-waiter-url
+    (let [num-messages 100
+          num-iterations 5]
+      (dotimes [iteration num-iterations]
+        (doseq [max-message-length [1000 10000 100000]]
+          (doseq [mode ["EXIT_PRE_RESPONSE" "EXIT_POST_RESPONSE"]]
+            (testing (str "lock-step mode " max-message-length " messages cancellations pre-response")
+              (let [{:keys [h2c-port host request-headers service-id]} (start-courier-instance waiter-url)]
+                (with-service-cleanup
+                  service-id
+                  (let [cancellation-index (* iteration (/ num-messages num-iterations))
+                        collect-cid (str (rand-name) "." mode "." cancellation-index "-" num-messages "." max-message-length)
+                        from (rand-name "f")
+                        ids (map #(str "id-" (cond-> % (= % cancellation-index) (str "." mode))) (range num-messages))
+                        messages (doall (repeatedly num-messages #(rand-str (inc (rand-int max-message-length)))))
+                        request-headers (assoc request-headers "x-cid" collect-cid)
+                        summaries (GrpcClient/collectPackages
+                                    host h2c-port request-headers ids from messages 1 true (inc num-messages))
+                        assertion-message (str {:cancellation-index cancellation-index
+                                                :collect-cid collect-cid
+                                                :iteration iteration
+                                                :service-id service-id
+                                                :summaries (map (fn [s]
+                                                                  {:num-messages (.getNumMessages s)
+                                                                   :total-length (.getTotalLength s)})
+                                                                summaries)})
+                        expected-summary-count (cond-> cancellation-index
+                                                 (= "EXIT_POST_RESPONSE" mode) inc)]
+                    (log/info "result" assertion-message) ;; TODO shams convert to log
+                    (is (= expected-summary-count (count summaries)) assertion-message)
+                    (when (seq summaries)
+                      (is (= (range 1 (inc expected-summary-count))
+                             (map #(.getNumMessages %) summaries))
+                          assertion-message)
+                      (is (= (reductions + (map count (take expected-summary-count messages)))
+                             (map #(.getTotalLength %) summaries))
+                          assertion-message))))))))))))
