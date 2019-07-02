acfd969b7111f082682aa747909167206165322b
Nikolai Sinkov <nsinkov@gmail.com>
2019-07-02 16:45:34 -0500

    change idp server to local server instead of docker

--- waiter/bin/ci/run-integration-tests-composite-scheduler.sh
+++ waiter/bin/ci/run-integration-tests-composite-scheduler.sh
@@ -15,21 +15,12 @@ TEST_COMMAND=${1:-parallel-test}
 TEST_SELECTOR=${2:-integration}
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-WAITER_DIR=${DIR}/../..
+export WAITER_DIR=${DIR}/../..
 TEST_APPS_DIR=${WAITER_DIR}/../containers/test-apps
 COURIER_DIR=${TEST_APPS_DIR}/courier
 KITCHEN_DIR=${TEST_APPS_DIR}/kitchen
 NGINX_DIR=${TEST_APPS_DIR}/nginx
 SEDIMENT_DIR=${TEST_APPS_DIR}/sediment
-
-# set SAML authenticator variables
-export SAML_IDP_URI="https://localhost:8443/simplesaml/saml2/idp/SSOService.php"
-export SAML_IDP_CERT_URI="${WAITER_DIR}/test-files/saml/idp.crt"
-export SAML_AUTH_USER="user2"
-if [[ $TEST_SELECTOR =~ fast$ ]]; then
-    # Start SAML IdP test server
-    ${DIR}/saml-idp-server-setup.sh
-fi
 
 # prepare courier server build
 pushd ${COURIER_DIR}
@@ -41,14 +32,24 @@ pushd ${SEDIMENT_DIR}
 mvn clean package
 popd
 
-# Start waiter
+# set SAML authenticator variables
+export SAML_IDP_PORT=8443
+export SAML_IDP_URI="http://localhost:${SAML_IDP_PORT}/"
+export SAML_IDP_CERT_URI="${WAITER_DIR}/test-files/saml/idp.crt"
 : ${WAITER_PORT:=9091}
+export WAITER_URI=127.0.0.1:${WAITER_PORT}
+if [[ $TEST_SELECTOR =~ fast$ ]]; then
+    # Start SAML IdP test server
+    ${DIR}/saml-idp-server-setup.sh
+fi
+
+# Start waiter
 ${WAITER_DIR}/bin/run-using-composite-scheduler.sh ${WAITER_PORT} &
+
 
 # Run the integration tests
 export WAITER_TEST_COURIER_CMD=${COURIER_DIR}/bin/run-courier-server.sh
 export WAITER_TEST_KITCHEN_CMD=${KITCHEN_DIR}/bin/kitchen
 export WAITER_TEST_NGINX_CMD=${NGINX_DIR}/bin/run-nginx-server.sh
 export WAITER_TEST_SEDIMENT_CMD=${SEDIMENT_DIR}/bin/run-sediment-server.sh
-export WAITER_URI=127.0.0.1:${WAITER_PORT}
 ${WAITER_DIR}/bin/test.sh ${TEST_COMMAND} ${TEST_SELECTOR}
