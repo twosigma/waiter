26914a4e9b09b3da746d2dd398ecd56b86698e53
Shams Imam <shams.imam@twosigma.com>
2019-07-25 09:05:36 -0500

    updates jet version for fix including response trailers and body streaming race

--- waiter/project.clj
+++ waiter/project.clj
@@ -43,7 +43,7 @@
                  [io.grpc/grpc-core "1.20.0"
                   :exclusions [com.google.guava/guava]
                   :scope "test"]
-                 [twosigma/jet "0.7.10-20190723_182557-gfa75f4c"
+                 [twosigma/jet "0.7.10-20190725_133040-g0c08fb4"
                   :exclusions [org.mortbay.jetty.alpn/alpn-boot]]
                  [twosigma/clj-http "1.0.2-20180124_201819-gcdf23e5"
                   :exclusions [commons-codec commons-io org.clojure/tools.reader potemkin slingshot]]
