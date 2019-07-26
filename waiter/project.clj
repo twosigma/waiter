2d5f01c0f2104e0f79e69ea9589b31e0b45305ea
Shams Imam <shams.imam@twosigma.com>
2019-07-26 01:23:07 -0500

    updates jet version

--- waiter/project.clj
+++ waiter/project.clj
@@ -43,7 +43,7 @@
                  [io.grpc/grpc-core "1.20.0"
                   :exclusions [com.google.guava/guava]
                   :scope "test"]
-                 [twosigma/jet "0.7.10-20190725_133505-gf1a6862"
+                 [twosigma/jet "0.7.10-20190726_062116-ge1f0a7b"
                   :exclusions [org.mortbay.jetty.alpn/alpn-boot]]
                  [twosigma/clj-http "1.0.2-20180124_201819-gcdf23e5"
                   :exclusions [commons-codec commons-io org.clojure/tools.reader potemkin slingshot]]
