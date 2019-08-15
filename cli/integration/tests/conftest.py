c4ce20544a56006917b4896379f4e0afb0fa3c3b
Nikolai Sinkov <nsinkov@gmail.com>
2019-08-15 10:41:20 -0500

    PR feedback

--- cli/integration/tests/conftest.py
+++ cli/integration/tests/conftest.py
@@ -8,17 +8,14 @@ if 'TEST_METRICS_URL' in os.environ:
     import datetime
     import getpass
     import json
-    import random
     import socket
+    import uuid
     from timeit import default_timer as timer
 
     from tests.waiter import util
 
     elastic_search_url = os.getenv('TEST_METRICS_URL').rstrip('/')
     logging.info(f'Sending test metrics to {elastic_search_url}')
-    #print("xxx initializng failed_tests")
-    #failed_tests = []
-    #print("xxx done initializng failed_tests")
     failed_tests_file = os.getenv('TEST_METRICS_FAILED_TESTS_FILE', None)
     if failed_tests_file is None:
         try:
@@ -27,17 +24,6 @@ if 'TEST_METRICS_URL' in os.environ:
             pass
         failed_tests_file = '.test_metrics/last_failed_tests'
 
-
-    def pytest_configure():
-        pytest.my_failed_tests = []
-    
-    @pytest.fixture(scope='session', autouse=True)
-    def session_hook():
-        # Executed before tests
-        yield
-        # Executed after tests
-        with open(failed_tests_file, 'w') as file_handle:
-            json.dump({'failed-tests': pytest.my_failed_tests}, file_handle)
 
     @pytest.fixture()
     def record_test_metric(request):
@@ -63,8 +49,7 @@ if 'TEST_METRICS_URL' in os.environ:
                 logging.warning('Unable to determine test result')
                 result = 'unknown'
             if result == 'failed':
-                pytest.my_failed_tests.append(request_node._nodeid)
-                with open(failed_tests_file + "__" + str(random.randint(0, 999999999999999999999999999999)), 'w') as file_handle:
+                with open(failed_tests_file + "__" + str(uuid.uuid1()), 'w') as file_handle:
                     file_handle.write(request_node._nodeid + "\n")
             metrics = {
                 'build-id': os.getenv('TEST_METRICS_BUILD_ID', None),
