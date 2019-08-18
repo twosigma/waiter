1ece1b250ab114b3a2422f26e9e2a493fd9316a2
Shams <shamsimam@users.noreply.github.com>
2019-08-17 19:43:04 -0500

    adds kubernetes pod name to the request log (#927)

--- waiter/test/waiter/request_log_test.clj
+++ waiter/test/waiter/request_log_test.clj
@@ -67,6 +67,7 @@
                             "server" "foo-bar"}
                   :instance {:host "instance-host"
                              :id "instance-id"
+                             :k8s/pod-name "test-pod-name"
                              :port 123}
                   :instance-proto "instance-proto"
                   :latest-service-id "latest-service-id"
@@ -82,6 +83,7 @@
             :instance-id "instance-id"
             :instance-port 123
             :instance-proto "instance-proto"
+            :k8s-pod-name "test-pod-name"
             :latest-service-id "latest-service-id"
             :metric-group "service-metric-group"
             :principal "principal@DOMAIN.COM"
