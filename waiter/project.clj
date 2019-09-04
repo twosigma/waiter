49a8ec936f8ce26005ee0be3ec192cac35adb0b9
Shams <shamsimam@users.noreply.github.com>
2019-09-04 10:49:23 +0600

    cleanup trailer support after jetty upgrade (#940)
    * uses jet that avoids the need to perform special handling for empty trailers in http/2 requests
    
    * uses jet that avoids the need to populate dummy trailers in response since jetty no longer sends an empty trailers frame

--- waiter/project.clj
+++ waiter/project.clj
@@ -41,7 +41,7 @@
                  [io.grpc/grpc-core "1.20.0"
                   :exclusions [com.google.guava/guava]
                   :scope "test"]
-                 [twosigma/jet "0.7.10-20190820_214226-g0c908dc"
+                 [twosigma/jet "0.7.10-20190831_055713-gf193d34"
                   :exclusions [org.mortbay.jetty.alpn/alpn-boot]]
                  [twosigma/clj-http "1.0.2-20180124_201819-gcdf23e5"
                   :exclusions [commons-codec commons-io org.clojure/tools.reader potemkin slingshot]]
