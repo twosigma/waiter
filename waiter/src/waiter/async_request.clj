71c1b1728b60aadf514ac2c9666d9918b4a37e12
Shams <shamsimam@users.noreply.github.com>
2019-10-30 09:59:06 -0500

    makes async-request-max-status-checks configurable (#986)

--- waiter/src/waiter/async_request.clj
+++ waiter/src/waiter/async_request.clj
@@ -27,8 +27,6 @@
   (:import (java.net ConnectException SocketTimeoutException URI URLEncoder)
            (java.util.concurrent TimeoutException)))
 
-(def ^:const ^:private max-status-checks-per-async-request 50)
-
 (def daemon-counter (metrics/waiter-counter "async" "monitor" "daemon"))
 
 (def in-flight-requests-counter (metrics/waiter-counter "async" "monitor" "request"))
@@ -160,6 +158,18 @@
   (let [encode #(if %1 (URLEncoder/encode %1 "UTF-8") (str %1))]
     (str prefix (encode request-id) "/" (encode router-id) "/" service-id "/" (str host) "/" (str port) location)))
 
+(defn sanitize-check-interval
+  "Computes the async-check-interval to use by restricting the total number of checks to be performed
+   to async-request-max-status-checks."
+  [async-request-timeout-ms async-check-interval-ms async-request-max-status-checks]
+  (let [sanitized-check-interval-ms (int (/ async-request-timeout-ms async-request-max-status-checks))]
+    (if (>= async-check-interval-ms sanitized-check-interval-ms)
+      async-check-interval-ms
+      (do
+        (log/info "increasing async check interval to" sanitized-check-interval-ms
+                  "from" async-check-interval-ms)
+        sanitized-check-interval-ms))))
+
 (defn post-process-async-request-response
   "Triggers execution of monitoring system for an async request.
    The function assumes location begins with a slash.
@@ -171,7 +181,7 @@
   (let [correlation-id (cid/get-correlation-id)
         status-endpoint (scheduler/end-point-url backend-proto host port location)
         _ (log/info "status endpoint for async request is" status-endpoint query-string)
-        {:keys [async-check-interval-ms async-request-timeout-ms]} request-properties
+        {:keys [async-check-interval-ms async-request-max-status-checks async-request-timeout-ms]} request-properties
         exit-chan (async/chan 1)]
     ;; register async request
     (swap! async-request-store-atom assoc request-id (assoc reason-map :exit-chan exit-chan))
@@ -193,13 +203,7 @@
               (complete-async-request-locally async-request-store-atom release-instance-fn request-id status))
             (request-still-active? []
               (contains? @async-request-store-atom request-id))]
-      (let [sanitized-check-interval-ms (int (/ async-request-timeout-ms max-status-checks-per-async-request))
-            check-interval-ms (if (>= async-check-interval-ms sanitized-check-interval-ms)
-                                async-check-interval-ms
-                                (do
-                                  (log/info "increasing async check interval to" sanitized-check-interval-ms
-                                            "from" async-check-interval-ms)
-                                  sanitized-check-interval-ms))]
+      (let [check-interval-ms (sanitize-check-interval async-request-timeout-ms async-check-interval-ms async-request-max-status-checks)]
         (monitor-async-request make-get-request-fn complete-async-request-fn request-still-active? status-endpoint
                                check-interval-ms async-request-timeout-ms correlation-id exit-chan)))
     ;; modify the location header in the response
