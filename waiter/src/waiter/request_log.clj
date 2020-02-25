3a4651ab5deaeaa48464070d5a33aedf36ec07a2
Nikolai Sinkov <nsinkov@gmail.com>
2020-02-25 10:07:26 -0600

    pr feedback

--- waiter/src/waiter/request_log.clj
+++ waiter/src/waiter/request_log.clj
@@ -93,11 +93,10 @@
 (defn log-request!
   "Log a request"
   [request response]
-  (let [redacted-request-fields-string (get-in response [:descriptor :service-description "env" "REDACTED_REQUEST_FIELDS"])]
-    (log (apply dissoc
-                (merge (request->context request) (response->context response))
-                (when-not (str/blank? redacted-request-fields-string)
-                  (map keyword (str/split redacted-request-fields-string #",")))))))
+  (let [redacted-request-fields-string (get-in response [:descriptor :service-description "env" "REDACTED_REQUEST_FIELDS"])
+        redacted-request-fields (when-not (str/blank? redacted-request-fields-string)
+                                  (map keyword (str/split redacted-request-fields-string #",")))]
+    (log (apply dissoc (merge (request->context request) (response->context response)) redacted-request-fields))))
 
 (defn wrap-log
   "Wraps a handler logging data from requests and responses."
