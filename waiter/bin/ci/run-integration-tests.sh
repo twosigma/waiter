a033e0bf6e228211da8ec4cd4d125c3f60f54ae4
Shams Imam <shams.imam@twosigma.com>
2020-06-10 16:11:23 -0500

    debug logs

--- waiter/bin/ci/run-integration-tests.sh
+++ waiter/bin/ci/run-integration-tests.sh
@@ -13,20 +13,20 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 WAITER_DIR=${DIR}/../..
 SCHEDULER="${1:-shell}-scheduler"
-SUBCMD="${DIR}/run-integration-tests-${SCHEDULER}.sh ${2:-eftest} ${3:-integration}"
+SUBCMD="timeout 1800s ${DIR}/run-integration-tests-${SCHEDULER}.sh ${2:-eftest} ${3:-integration}"
 
 # Start netcat to listen to a port. The Codahale Graphite reporter will be able to report without failing and spamming logs.
 export GRAPHITE_SERVER_PORT=5555
 nc -kl localhost $GRAPHITE_SERVER_PORT > /dev/null &
 ncat_pid=$!
 
-if [ "${TRAVIS}" == true ]; then
+if [[ "${TRAVIS}" == true ]]; then
     # Capture integration test command output into a log file
     mkdir -p ${WAITER_DIR}/log
     bash -x -c "${SUBCMD}" &> >(tee ${WAITER_DIR}/log/travis.log)
 
     # If there were failures, dump the logs
-    if [ $? -ne 0 ]; then
+    if [[ $? -ne 0 ]]; then
         echo 'Uploading logs...'
         ${DIR}/upload-logs.sh
         exit 1
