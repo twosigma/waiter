c2fb2c27ece9e076c450be790a56f337feba66cf
Shams Imam <shams.imam@twosigma.com>
2020-06-12 01:19:36 -0500

    upgrades to jetty 9.4.28.v20200408

--- waiter/project.clj
+++ waiter/project.clj
@@ -45,7 +45,7 @@
                  [io.grpc/grpc-core "1.20.0"
                   :exclusions [com.google.guava/guava]
                   :scope "test"]
-                 [twosigma/jet "0.7.10-20200504_104810-g200ef43"
+                 [twosigma/jet "0.7.10-20200601_151340-g7a25230"
                   :exclusions [org.mortbay.jetty.alpn/alpn-boot]]
                  [twosigma/clj-http "1.0.2-20180124_201819-gcdf23e5"
                   :exclusions [commons-codec commons-io org.clojure/tools.reader potemkin slingshot]]
