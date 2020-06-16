298877e23fbd18b8a8b8ecce52642523b386aa6b
Zaurbek Zhakupov <zoollhub@gmail.com>
2020-06-16 12:04:09 -0400

    whitespace shift 4 spaces responder.clj

--- waiter/src/waiter/state/responder.clj
+++ waiter/src/waiter/state/responder.clj
@@ -471,10 +471,10 @@
           response-code (if instance-not-allowed? :in-use :blacklisted)]
       {:current-state' (if (= :blacklisted response-code)
                          (-> current-state
-                               ; mark instance as blacklisted and set the expiry time
-                               (update-in [:instance-id->state instance-id] sanitize-instance-state)
-                               (update-in [:instance-id->state instance-id] update-status-tag-fn #(conj % :blacklisted))
-                               (update-state-by-blacklisting-instance-fn cid instance-id blacklist-period-ms))
+                           ; mark instance as blacklisted and set the expiry time
+                           (update-in [:instance-id->state instance-id] sanitize-instance-state)
+                           (update-in [:instance-id->state instance-id] update-status-tag-fn #(conj % :blacklisted))
+                           (update-state-by-blacklisting-instance-fn cid instance-id blacklist-period-ms))
                          current-state)
        :response-chan response-chan
        :response response-code})))
