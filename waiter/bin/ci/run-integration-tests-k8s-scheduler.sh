b902f484f50fcfa95ea81ba312e4b5d9ae328852
Zaurbek Zhakupov <zoollhub@gmail.com>
2020-07-15 15:39:49 -0400

    old run-integration-k8s

--- waiter/bin/ci/run-integration-tests-k8s-scheduler.sh
+++ waiter/bin/ci/run-integration-tests-k8s-scheduler.sh
@@ -14,31 +14,37 @@ set -e
 TEST_COMMAND=${1:-eftest}
 TEST_SELECTOR=${2:-integration}
 
-export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export WAITER_DIR=${DIR}/../..
-export CONTAINERS_DIR=${WAITER_DIR}/../containers
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+WAITER_DIR=${DIR}/../..
+CONTAINERS_DIR=${WAITER_DIR}/../containers
 
 # Start minikube
 ${DIR}/minikube-setup.sh
 
 # Start S3 test server
 ${DIR}/s3-server-setup.sh
-export S3SERVER_IP=$(docker inspect s3server | jq -r '.[0].NetworkSettings.Networks.bridge.IPAddress')
+S3SERVER_IP=$(docker inspect s3server | jq -r '.[0].NetworkSettings.Networks.bridge.IPAddress')
 export WAITER_S3_BUCKET=http://$S3SERVER_IP:8000/waiter-service-logs
 
 # Ensure we have the docker image for the pods
 ${CONTAINERS_DIR}/bin/build-docker-images.sh
 
 # start the JWKS server
-export JWKS_PORT=6666
-# ${WAITER_DIR}/bin/ci/jwks-server-setup.sh ${JWKS_PORT}
+JWKS_PORT=6666
+${WAITER_DIR}/bin/ci/jwks-server-setup.sh ${JWKS_PORT}
 export JWKS_SERVER_URL="http://127.0.0.1:${JWKS_PORT}/keys"
 export OIDC_AUTHORIZE_URL="http://127.0.0.1:${JWKS_PORT}/authorize"
 export OIDC_TOKEN_URL="http://127.0.0.1:${JWKS_PORT}/id-token"
 export WAITER_TEST_JWT_ACCESS_TOKEN_URL="http://127.0.0.1:${JWKS_PORT}/get-token?host={HOST}"
 
 # Start waiter
-export WAITER_PORT=9091
+: ${WAITER_PORT:=9091}
+${WAITER_DIR}/bin/run-using-k8s.sh ${WAITER_PORT} &
+
+# Start monitoring state of Kubernetes pods
+bash +x ${DIR}/monitor-pods.sh &
+
+# Run the integration tests
 export INTEGRATION_TEST_BAD_IMAGE="twosigma/does-not-exist"
 export INTEGRATION_TEST_CUSTOM_IMAGE="twosigma/integration"
 export INTEGRATION_TEST_CUSTOM_IMAGE_ALIAS="alias/integration"
@@ -50,9 +56,11 @@ export WAITER_TEST_NGINX_CMD=/opt/nginx/bin/run-nginx-server.sh
 export WAITER_TEST_SEDIMENT_CMD=/opt/sediment/bin/run-sediment-server.sh
 export WAITER_AUTH_RUN_AS_USER=${USER}
 export WAITER_URI=127.0.0.1:${WAITER_PORT}
-${WAITER_DIR}/bin/run-using-k8s.sh ${WAITER_PORT} &
+${WAITER_DIR}/bin/test.sh ${TEST_COMMAND} ${TEST_SELECTOR}
 
-# Start monitoring state of Kubernetes pods
-# bash +x ${DIR}/monitor-pods.sh &
-
-# Run the integration tests
+# If there were failures, dump the logs
+if [ "$test_failures" = true ]; then
+    echo "Uploading logs..."
+    ${WAITER_DIR}/bin/ci/upload_logs.sh
+    exit 1
+fi
