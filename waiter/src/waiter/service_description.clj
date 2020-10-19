1a900e0b4a38367a58720b3d2c9465699b453a0f
Kevin <kevin.tang@twosigma.com>
2020-10-19 09:56:39 -0500

    add schema definition for user metadata 'maintenance'

--- waiter/src/waiter/service_description.clj
+++ waiter/src/waiter/service_description.clj
@@ -118,6 +118,7 @@
    (s/optional-key "https-redirect") s/Bool
    (s/optional-key "owner") schema/non-empty-string
    (s/optional-key "stale-timeout-mins") (s/both s/Int (s/pred #(<= 0 % (t/in-minutes (t/hours 4))) 'at-most-4-hours))
+   (s/optional-key "maintenance") {(s/required-key "message") (s/conditional #(<= 1 (count %) 512) s/Str)}
    s/Str s/Any})
 
 (def ^:const service-required-keys (->> (keys service-description-schema)
@@ -157,7 +158,7 @@
 (def ^:const system-metadata-keys #{"cluster" "deleted" "last-update-time" "last-update-user" "previous" "root"})
 
 ; keys allowed in user metadata for tokens, these need to be distinct from service description keys
-(def ^:const user-metadata-keys #{"cors-rules" "fallback-period-secs" "https-redirect" "owner" "stale-timeout-mins"})
+(def ^:const user-metadata-keys #{"cors-rules" "fallback-period-secs" "https-redirect" "owner" "stale-timeout-mins", "maintenance"})
 
 ; keys allowed in metadata for tokens, these need to be distinct from service description keys
 (def ^:const token-metadata-keys (set/union system-metadata-keys user-metadata-keys))
