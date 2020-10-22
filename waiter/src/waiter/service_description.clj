38774242e6ab85c95b1c6470f0964b62f66859db
Kevin <kevin.tang@twosigma.com>
2020-10-22 09:48:45 -0500

    remove expires-at and make message required for maintenance schema

--- waiter/src/waiter/service_description.clj
+++ waiter/src/waiter/service_description.clj
@@ -116,13 +116,7 @@
                                    (s/optional-key "methods") (s/both (s/pred not-empty) [schema/http-method])}]
    (s/optional-key "fallback-period-secs") (s/both s/Int (s/pred #(<= 0 % (t/in-seconds (t/days 1))) 'at-most-1-day))
    (s/optional-key "https-redirect") s/Bool
-   (s/optional-key "maintenance") {(s/optional-key "message") (s/constrained s/Str #(<= 1 (count %) 512))
-                                   (s/required-key "expires-at") (s/conditional
-                                                                   #(= "*" %) s/Str
-                                                                   #(try
-                                                                      (du/str-to-date %)
-                                                                      (catch Exception _
-                                                                        false)) s/Str)}
+   (s/optional-key "maintenance") {(s/required-key "message") (s/constrained s/Str #(<= 1 (count %) 512))}
    (s/optional-key "owner") schema/non-empty-string
    (s/optional-key "stale-timeout-mins") (s/both s/Int (s/pred #(<= 0 % (t/in-minutes (t/hours 4))) 'at-most-4-hours))
    s/Str s/Any})
