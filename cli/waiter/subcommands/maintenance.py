e2ff233b78c5ded67590299d0f384056a0d20cb7
Kevin Tang <kevin.tang@twosigma.com>
2020-11-16 13:17:44 -0600

    split into helper functions

--- cli/waiter/subcommands/maintenance.py
+++ cli/waiter/subcommands/maintenance.py
@@ -8,73 +8,63 @@ from waiter.querying import get_cluster_with_token, get_token
 from waiter.util import guard_no_cluster, logging, print_info
 
 
-def check_maintenance(clusters, args):
+def _is_token_in_maintenance_mode(token_data):
+    return 'maintenance' in token_data
+
+
+def _get_existing_token_data(clusters, token_name):
     guard_no_cluster(clusters)
-    token_name = args['token']
     cluster = get_cluster_with_token(clusters, token_name)
-    existing_token_data, existing_token_etag = get_token(cluster, token_name)
-    maintenance_mode_active = 'maintenance' in existing_token_data
+    return cluster, get_token(cluster, token_name)
+
+
+def _update_token(cluster, token_name, existing_token_etag, body):
+    cluster_name = cluster['name']
+    cluster_url = cluster['url']
+    headers = {'If-Match': existing_token_etag}
+    params = {'token': token_name}
+    try:
+        resp = http_util.post(cluster, 'token', body, params=params, headers=headers)
+        process_post_result(resp)
+        return 0
+    except requests.exceptions.ReadTimeout as rt:
+        logging.exception(rt)
+        print_info(terminal.failed(
+            f'Encountered read timeout with {cluster_name} ({cluster_url}). The operation may have completed.'))
+        return 1
+    except IOError as ioe:
+        logging.exception(ioe)
+        reason = f'Cannot connect to {cluster_name} ({cluster_url})'
+        message = post_failed_message(cluster_name, reason)
+        print_info(f'{message}\n')
+
+
+def check_maintenance(clusters, args):
+    token_name = args['token']
+    existing_token_data, existing_token_etag = _get_existing_token_data(clusters, token_name)
+    maintenance_mode_active = _is_token_in_maintenance_mode(existing_token_data)
     print_info(f'{token_name} is {"" if maintenance_mode_active else "not "}in maintenance mode')
     return 0 if maintenance_mode_active else 1
 
 
 def start_maintenance(clusters, args):
-    guard_no_cluster(clusters)
     token_name = args['token']
-    cluster = get_cluster_with_token(clusters, token_name)
-    cluster_name = cluster['name']
-    cluster_url = cluster['url']
-    existing_token_data, existing_token_etag = get_token(cluster, token_name)
+    cluster, existing_token_data, existing_token_etag = _get_existing_token_data(clusters, token_name)
     json_body = existing_token_data
-    headers = {'If-Match': existing_token_etag}
-    params = {'token': token_name}
-    try:
-        update_doc = {"maintenance": {"message": args['message']}}
-        json_body.update(update_doc)
-        resp = http_util.post(cluster, 'token', json_body, params=params, headers=headers)
-        process_post_result(resp)
-        return 0
-    except requests.exceptions.ReadTimeout as rt:
-        logging.exception(rt)
-        print_info(terminal.failed(
-            f'Encountered read timeout with {cluster_name} ({cluster_url}). The operation may have completed.'))
-        return 1
-    except IOError as ioe:
-        logging.exception(ioe)
-        reason = f'Cannot connect to {cluster_name} ({cluster_url})'
-        message = post_failed_message(cluster_name, reason)
-        print_info(f'{message}\n')
+    update_doc = {"maintenance": {"message": args['message']}}
+    json_body.update(update_doc)
+    return _update_token(cluster, token_name, existing_token_etag, json_body)
 
 
 def stop_maintenance(clusters, args):
-    guard_no_cluster(clusters)
     token_name = args['token']
-    cluster = get_cluster_with_token(clusters, token_name)
-    cluster_name = cluster['name']
-    cluster_url = cluster['url']
-    existing_token_data, existing_token_etag = get_token(cluster, token_name)
-    maintenance_mode_active = 'maintenance' in existing_token_data
+    cluster, existing_token_data, existing_token_etag = _get_existing_token_data(clusters, token_name)
+    maintenance_mode_active = _is_token_in_maintenance_mode(existing_token_data)
+    if not maintenance_mode_active:
+        raise Exception("Token is not in maintenance mode")
     json_body = existing_token_data
-    headers = {'If-Match': existing_token_etag}
-    params = {'token': token_name}
-    try:
-        if maintenance_mode_active:
-            json_body.pop("maintenance")
-        else:
-            raise Exception("Token is not in maintenance mode")
-        resp = http_util.post(cluster, 'token', json_body, params=params, headers=headers)
-        process_post_result(resp)
-        return 0
-    except requests.exceptions.ReadTimeout as rt:
-        logging.exception(rt)
-        print_info(terminal.failed(
-            f'Encountered read timeout with {cluster_name} ({cluster_url}). The operation may have completed.'))
-        return 1
-    except IOError as ioe:
-        logging.exception(ioe)
-        reason = f'Cannot connect to {cluster_name} ({cluster_url})'
-        message = post_failed_message(cluster_name, reason)
-        print_info(f'{message}\n')
+    json_body.pop("maintenance")
+    return _update_token(cluster, token_name, existing_token_etag, json_body)
 
 
 def maintenance(parser, clusters, args, _):
@@ -87,26 +77,28 @@ def maintenance(parser, clusters, args, _):
         return sub_func(clusters, args)
 
 
-def register_action(add_parser, sub_action, sub_func):
-    parser = add_parser(sub_action, help=f'{sub_action} maintenance mode for a token')
-    parser.add_argument('token')
-    parser.set_defaults(sub_func=sub_func)
-    return parser
-
-
 def register_check(add_parser):
-    register_action(add_parser, 'check', check_maintenance)
+    parser = add_parser('check',
+                        help='Checks if a token is in maintenance mode. Returns cli code 0 if the token is in '
+                             'maintenance mode and 1 if the token not in maintenance mode')
+    parser.add_argument('token')
+    parser.set_defaults(sub_func=check_maintenance)
 
 
 def register_stop(add_parser):
-    register_action(add_parser, 'stop', stop_maintenance)
+    parser = add_parser('stop', 'Stop maintenance mode for a token. Requests will be handled normally.')
+    parser.add_argument('token')
+    parser.set_defaults(sub_func=stop_maintenance)
 
 
 def register_start(add_parser):
-    start_parser = register_action(add_parser, 'start', start_maintenance)
-    start_parser.add_argument('message',
-                              help='Your message will be provided in a 503 response for requests to the token. '
-                                   'The message cannot be more than 512 characters.')
+    parser = add_parser('start', 'Start maintenance mode for a token. All requests to this token will begin to receive '
+                                 'a 503 response.')
+    parser.add_argument('token')
+    parser.add_argument('message',
+                        help='Your message will be provided in a 503 response for requests to the token. '
+                             'The message cannot be more than 512 characters.')
+    parser.set_defaults(sub_func=start_maintenance)
 
 
 def register(add_parser):
