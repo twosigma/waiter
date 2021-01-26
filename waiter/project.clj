88831e3fa2fd7ff3cf3f4cb67eb14a68f1659d36
Shams <shamsimam@users.noreply.github.com>
2021-01-26 12:14:21 -0600

    adds keystore file hot-reload support (#1274)

--- waiter/project.clj
+++ waiter/project.clj
@@ -45,7 +45,7 @@
                  [io.grpc/grpc-core "1.20.0"
                   :exclusions [com.google.guava/guava]
                   :scope "test"]
-                 [twosigma/jet "0.7.10-20200801_013034-g6cffb81"
+                 [twosigma/jet "0.7.10-20210126_171807-gea4b804"
                   :exclusions [org.mortbay.jetty.alpn/alpn-boot]]
                  [twosigma/clj-http "1.0.2-20180124_201819-gcdf23e5"
                   :exclusions [commons-codec commons-io org.clojure/tools.reader potemkin slingshot]]
