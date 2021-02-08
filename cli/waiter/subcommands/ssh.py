252cd32515bc82380b37244a6e0ab42a11d6e247
Kevin Tang <kevin.tang@twosigma.com>
2021-02-08 11:31:52 -0600

    add command possibility

--- cli/waiter/subcommands/ssh.py
+++ cli/waiter/subcommands/ssh.py
@@ -1,6 +1,14 @@
+import argparse
+import os
+
 def ssh(clusters, args, _, __):
     # should I even allow ssh into non kubernetes cluster?
     # not on kubernetes ssh in marathon
+    os.execlp('kubectl', 'kubectl',
+              'exec',
+              '-c', os.getenv('COOK_CONTAINER_NAME_FOR_JOB', 'required-cook-job-container'),
+              '-it', instance_uuid,
+              '--', '/bin/sh', '-c', 'cd $HOME; exec /bin/sh')
 
     return 0
 
@@ -14,4 +22,5 @@ def register(add_parser):
     id_group.add_argument('--token', '-t', dest='is-token', action='store_true')
     id_group.add_argument('--service-id', '-s', dest='is-service-id', action='store_true')
     id_group.add_argument('--pod-name', '-p', dest='is-pod-name', action='store_true')
+    parser.add_argument('command', nargs=argparse.REMAINDER)
     return ssh
