fcfeb2a0c3f2e3d0906c7eb46dbd351fa0db9af6
Kevin Tang <kevin.tang@twosigma.com>
2021-02-19 17:17:18 -0600

    remove unecessary logging

--- cli/waiter/plugins.py
+++ cli/waiter/plugins.py
@@ -1,12 +1,10 @@
-import logging
-
 __plugins = {}
+
 
 def configure(plugins):
     """Configures global plugins to the plugins map"""
     global __plugins
     __plugins = plugins
-    logging.debug('plugins: %s', __plugins)
 
 
 def get_fn(plugin_name, default_fn):
