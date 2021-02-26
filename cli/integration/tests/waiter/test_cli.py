0d979d566c5b991a34c8b9ad60061ec45ca9385f
Kevin Tang <kevin.tang@twosigma.com>
2021-02-26 09:18:04 -0600

    add custom container test

--- cli/integration/tests/waiter/test_cli.py
+++ cli/integration/tests/waiter/test_cli.py
@@ -1578,9 +1578,9 @@ class WaiterCliTest(util.WaiterTest):
         self.assertEqual(0, cp.returncode, cp.stderr)
         self.assertEqual(cli.stdout(cp_help), cli.stdout(cp))
 
-    def __test_ssh(self, instance_fn, command_to_run=None, stdin=None, min_instances=1, is_failed_instance=False,
-                   test_service=False, test_instance=False, multiple_services=False, quick=False, expect_no_data=False,
-                   expect_out_of_range=False):
+    def __test_ssh(self, instance_fn, command_to_run=None, stdin=None, min_instances=1, admin=False,
+                   container_name=None, is_failed_instance=False, test_service=False, test_instance=False,
+                   multiple_services=False, quick=False, expect_no_data=False, expect_out_of_range=False):
         token_name = self.token_name()
         token_fields = util.minimal_service_description()
         token_fields['min-instances'] = min_instances
@@ -1605,23 +1605,28 @@ class WaiterCliTest(util.WaiterTest):
                 token_new_fields['cmd'] = 'this_is_an_invalid_command2'
                 util.post_token(self.waiter_url, token_name, token_new_fields)
                 util.ping_token(self.waiter_url, token_name, expected_status_code=503)
-
-            # ssh into instance
             env = os.environ.copy()
-            env["WAITER_SSH"] = 'echo'
-            env["WAITER_KUBECTL"] = 'echo'
+            env['WAITER_SSH'] = 'echo'
+            env['WAITER_KUBECTL'] = 'echo'
+            if admin:
+                env['WAITER_ADMIN'] = 'true'
             instance = instance_fn(service_id, instances)
-            quick_flag = ' -q' if quick else ''
+            ssh_flags = []
+            if quick:
+                ssh_flags.append('-q')
+            if container_name:
+                ssh_flags.append(f'--container-name {container_name}')
             if test_instance:
-                cp = cli.ssh(self.waiter_url, instance['id'], stdin=stdin, ssh_command=command_to_run,
-                             ssh_flags='-i' + quick_flag, env=env)
+                ssh_dest = instance['id']
+                ssh_flags.append('-i')
             elif test_service:
-                cp = cli.ssh(self.waiter_url, service_id, stdin=stdin, ssh_command=command_to_run,
-                             ssh_flags='-s' + quick_flag, env=env)
+                ssh_dest = service_id
+                ssh_flags.append('-s')
             else:
-                cp = cli.ssh(self.waiter_url, token_name, stdin=stdin, ssh_command=command_to_run, ssh_flags=quick_flag,
-                             env=env)
-
+                ssh_dest = token_name
+            cp = cli.ssh(self.waiter_url, ssh_dest, stdin=stdin, ssh_command=command_to_run,
+                         ssh_flags=' '.join(ssh_flags),
+                         env=env)
             if expect_out_of_range:
                 self.assertEqual(1, cp.returncode, cp.stderr)
                 self.assertIn('Input is out of range!', cli.stderr(cp))
@@ -1632,7 +1637,7 @@ class WaiterCliTest(util.WaiterTest):
                 log_directory = instance['log-directory']
                 self.assertEqual(0, cp.returncode, cp.stderr)
                 if util.using_kubernetes(self.waiter_url):
-                    container_name = 'waiter-app'
+                    container_name = container_name or 'waiter-app'
                     api_server = instance['k8s/api-server-url']
                     namespace = instance['k8s/namespace']
                     pod_name = instance['k8s/pod-name']
@@ -1710,6 +1715,7 @@ class WaiterCliTest(util.WaiterTest):
     def test_ssh_token_multiple_services_instances_quick(self):
         self.__test_ssh(lambda _, instances: instances['active-instances'][0], min_instances=2, multiple_services=True,
                         quick=True)
-    #
-    # def test_ssh_custom_container(self):
-    #     self.assertTrue(False)
+
+    def test_ssh_token_custom_container(self):
+        self.__test_ssh(lambda _, instances: instances['active-instances'][0], admin=True,
+                        container_name='waiter-files')
