43b196b7ba7ea31b6e1f29320cb07b8db22cac9c
Kevin Tang <kevin.tang@twosigma.com>
2021-04-30 15:05:59 -0500

    test instanceEventHandler state fn

--- waiter/integration/waiter/instance_tracker_integration_test.clj
+++ waiter/integration/waiter/instance_tracker_integration_test.clj
@@ -1,5 +1,6 @@
 (ns waiter.instance-tracker-integration-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.set :as set]
+            [clojure.test :refer :all]
             [waiter.util.client-tools :refer :all]))
 
 (defn- get-instance-tracker-state
@@ -24,4 +25,14 @@
           (is (= (set (keys state))
                  default-state-fields))
           (is (= (set supported-include-params)
-                 #{"id->failed-instance" "instance-failure-handler"})))))))
+                 #{"id->failed-instance" "instance-failure-handler"})))))
+
+    (testing "InstanceEventHandler provides default state fields"
+      (let [query-params "include=instance-failure-handler"
+            {:keys [body] :as response} (get-instance-tracker-state waiter-url :query-params query-params)
+            default-inst-event-handler-state-fields #{"last-error-time" "supported-include-params" "type"}]
+        (assert-response-status response 200)
+        (let [body-json (try-parse-json body)
+              state (get-in body-json ["state" "instance-failure-handler"] #{"!@#$#^!#$13"})]
+          (is (set/superset? (set (keys state))
+                             default-inst-event-handler-state-fields)))))))
