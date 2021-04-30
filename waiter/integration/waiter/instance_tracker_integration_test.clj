96523db02b388fea394f0f84e87c1073dcbc4169
Kevin Tang <kevin.tang@twosigma.com>
2021-04-30 17:31:54 -0500

    integration test for failed instance tracking

--- waiter/integration/waiter/instance_tracker_integration_test.clj
+++ waiter/integration/waiter/instance_tracker_integration_test.clj
@@ -1,38 +1,80 @@
 (ns waiter.instance-tracker-integration-test
   (:require [clojure.set :as set]
             [clojure.test :refer :all]
+            [clojure.tools.logging :as log]
+            [clojure.walk :as walk]
+            [waiter.status-codes :refer :all]
             [waiter.util.client-tools :refer :all]))
 
 (defn- get-instance-tracker-state
-  [waiter-url & {:keys [cookies query-params]}]
-  (make-request waiter-url "/state/instance-tracker"
-                :cookies cookies
-                :query-params query-params
-                :method :get))
+  [waiter-url & {:keys [cookies keywordize-keys query-params]
+                 :or {keywordize-keys true}}]
+  (let [response (make-request waiter-url "/state/instance-tracker"
+                               :cookies cookies
+                               :query-params query-params
+                               :method :get)]
+    (assert-response-status response http-200-ok)
+    (cond-> (some-> response :body try-parse-json)
+            keywordize-keys walk/keywordize-keys)))
 
-(deftest ^:parallel ^:integration-fast test-instance-tracker-daemon
+(deftest ^:parallel ^:integration-fast test-instance-tracker-daemon-state
   (testing-using-waiter-url
-    (let [{:keys [cookies]} (make-request waiter-url "/waiter-auth")
-          routers (routers waiter-url)
-          router-urls (vals routers)])
+    (let [{:keys [cookies]} (make-request waiter-url "/waiter-auth")]
 
-    (testing "no query parameters provides the default state fields"
-      (let [{:keys [body] :as response} (get-instance-tracker-state waiter-url)
-            default-state-fields #{"last-update-time" "supported-include-params"}]
-        (assert-response-status response 200)
-        (let [{:strs [state]} (try-parse-json body)
-              {:strs [supported-include-params]} state]
+      (testing "no query parameters provides the default state fields"
+        (let [{{:keys [supported-include-params] :as state} :state} (get-instance-tracker-state waiter-url :cookies cookies)
+              default-state-fields #{:last-update-time :supported-include-params}]
           (is (= (set (keys state))
                  default-state-fields))
           (is (= (set supported-include-params)
-                 #{"id->failed-instance" "instance-failure-handler"})))))
+                 #{"id->failed-instance" "instance-failure-handler"}))))
 
-    (testing "InstanceEventHandler provides default state fields"
-      (let [query-params "include=instance-failure-handler"
-            {:keys [body] :as response} (get-instance-tracker-state waiter-url :query-params query-params)
-            default-inst-event-handler-state-fields #{"last-error-time" "supported-include-params" "type"}]
-        (assert-response-status response 200)
-        (let [body-json (try-parse-json body)
-              state (get-in body-json ["state" "instance-failure-handler"] #{"!@#$#^!#$13"})]
-          (is (set/superset? (set (keys state))
+      (testing "InstanceEventHandler provides default state fields"
+        (let [query-params "include=instance-failure-handler"
+              body (get-instance-tracker-state waiter-url
+                                               :cookies cookies
+                                               :query-params query-params)
+              default-inst-event-handler-state-fields #{:last-error-time :supported-include-params :type}]
+          (is (set/superset? (set (keys (get-in body [:state :instance-failure-handler] #{"!@#!@#14132"})))
                              default-inst-event-handler-state-fields)))))))
+
+(deftest ^:parallel ^:integration-fast ^:resource-heavy test-instance-tracker-failing-instance
+  (testing-using-waiter-url
+    (let [{:keys [cookies]} (make-request waiter-url "/waiter-auth")]
+
+      ; TODO: When instance tracking becomes more reliable in multi router scenarios this test should not be
+      ; on the same router
+      (testing "new failing instances appear in instance-tracker state on same router"
+        (let [router-url (rand-router-url waiter-url)
+              {:keys [service-id] :as response}
+              (make-request-with-debug-info
+                {:x-waiter-cmd "invalidcmd34sdfadsve"
+                 :x-waiter-name (rand-name)}
+                #(make-kitchen-request router-url % :cookies cookies))]
+          (with-service-cleanup
+            service-id
+            (assert-response-status response http-503-service-unavailable)
+            (is (wait-for
+                  (fn []
+                    (let [{:keys [failed-instances]} (:instances (service-settings router-url service-id :cookies cookies))]
+                      (< 0 (count failed-instances))))))
+            (let [{:keys [failed-instances]} (:instances (service-settings router-url service-id :cookies cookies))]
+              (log/info "The failed instances should be tracked by the instance-tracker" {:failed-instances failed-instances})
+              (is (< 0 (count failed-instances)))
+              (let [query-params "include=instance-failure-handler&include=recent-id->failed-instance-date&include=id->failed-instance"
+                    {{:keys [id->failed-instance]
+                      {:keys [type recent-id->failed-instance-date]} :instance-failure-handler} :state}
+                    (get-instance-tracker-state router-url
+                                                :cookies cookies
+                                                :query-params query-params)
+                    daemon-failed-ids (set (keys id->failed-instance))
+                    default-event-handler-ids (set (keys recent-id->failed-instance-date))]
+                ; assert failed instances are tracked by daemon
+                (doseq [{:keys [id]} failed-instances]
+                  (is (contains? daemon-failed-ids
+                                 (keyword id)))
+                  ; assert failed instances are tracked by DefaultInstanceFailureHandler cache of new failed instances
+                  (when (= type "DefaultInstanceFailureHandler")
+                    (is (contains? default-event-handler-ids
+                                   (keyword id)))))))))))))
+
