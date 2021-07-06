85ad60210401f4e3b5a28bbf5a73a0e3d233a85b
Kevin Tang <kevin.tang@twosigma.com>
2021-07-06 10:36:00 -0500

    Fix CLI parsing non string values for "env" and "metadata" fields (#1376)
    * prevent parsing certain fields as boolean values
    
    * add test for env and metadata being parsed only as strings
    
    * remove unecessary print statements
    
    * env and metadata should be parsed as strings even if suffix match int or float parsing

--- cli/waiter/token_post.py
+++ cli/waiter/token_post.py
@@ -14,6 +14,7 @@ from waiter.util import deep_merge, FALSE_STRINGS, is_admin_enabled, print_info,
 BOOL_STRINGS = TRUE_STRINGS + FALSE_STRINGS
 INT_PARAM_SUFFIXES = ['-failures', '-index', '-instances', '-length', '-level', '-mins', '-secs']
 FLOAT_PARAM_SUFFIXES = ['-factor', '-rate', '-threshold']
+STRING_PARAM_PREFIXES = ['env', 'metadata']
 
 
 class Action(Enum):
@@ -152,7 +153,6 @@ def create_or_update_token(clusters, args, _, enforce_cluster, action):
                             f'without specifying the --override flag.')
         else:
             logging.debug(f'Following parameters have specified values in both file and flags: {overrides}')
-
     token_fields = merge_token_fields_from_args(token_fields_from_json, token_fields_from_args)
     token_name_from_json = token_fields.pop('token', None)
     if token_name_from_args and token_name_from_json:
@@ -272,7 +272,10 @@ def add_implicit_arguments(unknown_args, parser):
     for i in range(num_unknown_args):
         arg = unknown_args[i]
         if arg.startswith(("-", "--")):
-            if any(arg.endswith(suffix) for suffix in INT_PARAM_SUFFIXES):
+            arg_dest = arg.lstrip('-')
+            if any(arg_dest.startswith(prefix) for prefix in STRING_PARAM_PREFIXES):
+                arg_type = None
+            elif any(arg.endswith(suffix) for suffix in INT_PARAM_SUFFIXES):
                 arg_type = possible_int
             elif any(arg.endswith(suffix) for suffix in FLOAT_PARAM_SUFFIXES):
                 arg_type = possible_float
@@ -280,4 +283,4 @@ def add_implicit_arguments(unknown_args, parser):
                 arg_type = str2bool
             else:
                 arg_type = None
-            parser.add_argument(arg, dest=arg.lstrip('-'), type=arg_type)
+            parser.add_argument(arg, dest=arg_dest, type=arg_type)
