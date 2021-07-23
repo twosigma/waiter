794f866a682091a9ba984fe6111f47c89be48816
Nick Vrvilo <Nick.Vrvilo@rice.edu>
2021-07-23 09:05:41 -0500

    Delete unused Cook scheduler (#1398)
    The Waiter-on-Cook implementation was never productionalized.
    Removing it now in order to reduce support/maintenance burden.

--- waiter/integration/waiter/basic_test.clj
+++ waiter/integration/waiter/basic_test.clj
@@ -1305,8 +1305,7 @@
             (let [kitchen-image (System/getenv "INTEGRATION_TEST_KITCHEN_IMAGE")
                   _ (is (not (str/blank? kitchen-image)) "You must provide a kitchen image in the INTEGRATION_TEST_KITCHEN_IMAGE environment variable")]
               (make-kitchen-request-fn kitchen-image http-200-ok))
-            (or (using-cook? waiter-url)
-                (using-shell? waiter-url))
+            (using-shell? waiter-url)
             (make-kitchen-request-fn "dummy/image" http-500-internal-server-error)))))
 
 (deftest ^:parallel ^:integration-fast test-self-service-cors
