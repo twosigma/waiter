549fcb4143cb17c817d0adbf549e600a3fafe384
Shams <shamsimam@users.noreply.github.com>
2021-07-26 09:30:58 -0500

    formatting changes

--- waiter/test/waiter/service_description_test.clj
+++ waiter/test/waiter/service_description_test.clj
@@ -2202,7 +2202,7 @@
     (testing "testing invalid backend proto and health check proto combination"
       (let [supported-protocols #{"http" "https" "h2c" "h2"}]
         (doseq [backend-proto supported-protocols
-              health-check-proto (disj supported-protocols backend-proto)]
+                health-check-proto (disj supported-protocols backend-proto)]
           (do
             (run-validate-schema-test
               (assoc valid-description
