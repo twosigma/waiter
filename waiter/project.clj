8428d69d435da7ff6ed6f0ed5314c59e5a809d55
Shams <shams.imam@twosigma.com>
2022-03-29 21:56:48 -0500

    updates jet to include websocket backpressure fix

--- waiter/project.clj
+++ waiter/project.clj
@@ -45,7 +45,7 @@
                  [io.grpc/grpc-core "1.20.0"
                   :exclusions [com.google.guava/guava]
                   :scope "test"]
-                 [twosigma/jet "0.7.10-20210727_221451-g7742bea"
+                 [twosigma/jet "0.7.10-20220331_133346-g2ad6b07"
                   :exclusions [org.mortbay.jetty.alpn/alpn-boot]]
                  [twosigma/clj-http "1.0.2-20180124_201819-gcdf23e5"
                   :exclusions [commons-codec commons-io org.clojure/tools.reader potemkin slingshot]]
