c3ac9efcd00920549a99d1cb409c1fd1451897e2
Derik Wang <derik0194@gmail.com>
2022-11-14 20:08:20 -0600

    updated signal cli command for token, service-id, and instance-id

--- cli/waiter/subcommands/signal.py
+++ cli/waiter/subcommands/signal.py
@@ -1,18 +1,61 @@
 from waiter.util import guard_no_cluster, check_positive
 from waiter.action import process_signal_request
-from waiter.display import get_user_selection, tabulate_service_instances
-from waiter.querying import query_service
+from waiter.display import get_user_selection, tabulate_service_instances, tabulate_token_services
+from waiter.querying import print_no_data, print_no_services, query_service, query_token, print_no_instances
 from enum import Enum
+
 
 class Signal(Enum):
     SIGKILL = 'sigkill'
     SIGTERM = 'sigterm'
 
+class Destination(Enum):
+    TOKEN = 'token'
+    SERVICE_ID = 'service_id'
+    INSTANCE_ID = 'instance_id'
+
+def signal_service_id(clusters, service_id):
+    print("hello")
+    instances = get_instances_from_service_id(clusters, service_id)
+    if instances is False:
+        print_no_data(clusters)
+        return 1
+    if len(instances) == 0:
+        print_no_instances(service_id)
+        return 1
+    else:
+        column_names = ['Instance Id', 'Host', 'Status']
+        tabular_output = tabulate_service_instances(instances, show_index=True, column_names=column_names)
+        selected_instance = get_user_selection(instances, tabular_output)
+    return selected_instance
+
+def signal_token(clusters, token):
+    query_result = query_token(clusters, token, include_services=True)
+    if query_result['count'] == 0:
+        print_no_data(clusters)
+        return 1
+    clusters_by_name = {c['name']: c for c in clusters}
+    cluster_data = query_result['clusters']
+    services = [{'cluster': cluster, 'etag': data['etag'], **service}
+                for cluster, data in cluster_data.items()
+                for service in data['services']]
+    if len(services) == 0:
+        print_no_services(clusters, token)
+        return 1
+    column_names = ['Service Id', 'Cluster', 'Instances', 'In-flight req.', 'Status', 'Last request', 'Current?']
+    tabular_output, sorted_services = tabulate_token_services(services, token, show_index=True, summary_table=False,
+                                                                column_names=column_names)
+    selected_service = get_user_selection(sorted_services, tabular_output)
+    selected_service_id = selected_service['service-id']
+    clusters = [clusters_by_name[selected_service['cluster']]]
+    return signal_service_id(clusters, selected_service_id)
+
+
 def map_instances_with_status(instances, status):
     return [{'_status': status, **inst} for inst in instances]
 
-def get_instances_from_service_id(clusters, service_id, include_active_instances, include_failed_instances,
-                                  include_killed_instances):
+
+def get_instances_from_service_id(clusters, service_id):
     query_result = query_service(clusters, service_id)
     num_services = query_result['count']
     if num_services == 0:
@@ -20,29 +63,28 @@ def get_instances_from_service_id(clusters, service_id, include_active_instances
     services = map(lambda service_data: service_data['service'], query_result['clusters'].values())
     instances = []
     for service in services:
-        if include_active_instances:
-            instances += map_instances_with_status(service['instances']['active-instances'], 'active')
-        if include_failed_instances:
-            instances += map_instances_with_status(service['instances']['failed-instances'], 'failed')
-        if include_killed_instances:
-            instances += map_instances_with_status(service['instances']['killed-instances'], 'killed')
+        instances += map_instances_with_status(service['instances']['active-instances'], 'active')
     return instances
 
 def signal(clusters, args, _, __):
     guard_no_cluster(clusters)
 
-    service_id = args.pop('service-id')
+    token_or_service_id_or_instance_id = args.pop('token-or-service-id-or-instance-id')
 
-    instances = get_instances_from_service_id(clusters, service_id, True, False, False)
-    column_names = ['Instance Id', 'Host', 'Status']
-    tabular_output = tabulate_service_instances(instances, show_index=True, column_names=column_names)
-    selected_instance = get_user_selection(instances, tabular_output)
+    signal_destination = args.pop('signal_destination')
+
+    instance_id = None
+    if signal_destination == Destination.TOKEN:
+        selected_instance = signal_token(clusters, token_or_service_id_or_instance_id)
+        instance_id = selected_instance.get('id')
+    elif signal_destination == Destination.SERVICE_ID:
+        selected_instance = signal_service_id(clusters, token_or_service_id_or_instance_id)
+        instance_id = selected_instance.get('id')
+    elif signal_destination == Destination.INSTANCE_ID:
+        instance_id = token_or_service_id_or_instance_id
     
-    instance_id = selected_instance.get('id')
     signal_type = args.pop('signal_type')
-
     timeout_secs = args['timeout']
-
     success = False
 
     if signal_type == Signal.SIGKILL.value:
@@ -57,9 +99,18 @@ def register(add_parser):
     """Adds this sub-command's parser and returns the action function"""
     parser = add_parser('signal', help='sends signal to instance')
     parser.add_argument('signal_type', help='type of signal to send to instance')
-    # parser.add_argument('instance-id')
-    parser.add_argument('service-id')
-    parser.add_argument('--timeout', '-t', help='timeout (in seconds) for kill to complete',
-                        type=check_positive, default=30)
+
+    parser.add_argument('token-or-service-id-or-instance-id')
+    id_group = parser.add_mutually_exclusive_group(required=False)
+    id_group.add_argument('--token', '-t', dest='signal_destination', action='store_const', const=Destination.TOKEN,
+                          default=Destination.TOKEN, help='Default; signal with token')
+    id_group.add_argument('--service-id', '-s', dest='signal_destination', action='store_const',
+                          const=Destination.SERVICE_ID, help='signal using a service id')
+    id_group.add_argument('--instance-id', '-i', dest='signal_destination', action='store_const',
+                          const=Destination.INSTANCE_ID, help='signal directly to instance id')
+
+
+    parser.add_argument('--timeout', '-ti', help='timeout (in seconds) for kill to complete',
+                        type=check_positive, default=30)    
 
     return signal
